283058e275e56567bf00e9894d123b9c68afb7c0
Damontecres <154766448+damontecres@users.noreply.github.com>
2023-12-31 01:14:30 -0500

    Increment version code for a release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.github.damontecres.stashapp"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = getAppVersion()
     }
 
