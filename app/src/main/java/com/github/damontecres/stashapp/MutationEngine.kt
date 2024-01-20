f4864e7942266270c1a599e1282080a84038ab11
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-01-20 17:54:23 -0500

    Add triggering a library scan & generate task

--- app/src/main/java/com/github/damontecres/stashapp/MutationEngine.kt
+++ app/src/main/java/com/github/damontecres/stashapp/MutationEngine.kt
@@ -5,10 +5,15 @@ import android.util.Log
 import android.widget.Toast
 import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.api.Mutation
+import com.apollographql.apollo3.api.Optional
 import com.apollographql.apollo3.exception.ApolloException
 import com.apollographql.apollo3.exception.ApolloHttpException
 import com.apollographql.apollo3.exception.ApolloNetworkException
+import com.github.damontecres.stashapp.api.MetadataGenerateMutation
+import com.github.damontecres.stashapp.api.MetadataScanMutation
 import com.github.damontecres.stashapp.api.SceneSaveActivityMutation
+import com.github.damontecres.stashapp.api.type.GenerateMetadataInput
+import com.github.damontecres.stashapp.api.type.ScanMetadataInput
 
 /**
  * Class for sending graphql mutations
@@ -16,6 +21,8 @@ import com.github.damontecres.stashapp.api.SceneSaveActivityMutation
 class MutationEngine(private val context: Context, private val showToasts: Boolean = false) {
     private val client =
         createApolloClient(context) ?: throw QueryEngine.StashNotConfiguredException()
+
+    private val serverPreferences = ServerPreferences(context)
 
     private suspend fun <D : Mutation.Data> executeMutation(mutation: Mutation<D>): ApolloResponse<D> {
         val mutationName = mutation.name()
@@ -85,6 +92,51 @@ class MutationEngine(private val context: Context, private val showToasts: Boole
             SceneSaveActivityMutation(scene_id = sceneId.toString(), resume_time = resumeTime)
         val result = executeMutation(mutation)
         return result.data!!.sceneSaveActivity
+    }
+
+    private fun getServerBoolean(preferenceKey: String): Optional<Boolean> {
+        return Optional.present(
+            serverPreferences.preferences.getBoolean(
+                preferenceKey,
+                false,
+            ),
+        )
+    }
+
+    suspend fun triggerScan() {
+        val mutation =
+            MetadataScanMutation(
+                ScanMetadataInput(
+                    scanGenerateClipPreviews = getServerBoolean(ServerPreferences.PREF_SCAN_GENERATE_CLIP_PREVIEWS),
+                    scanGenerateCovers = getServerBoolean(ServerPreferences.PREF_SCAN_GENERATE_COVERS),
+                    scanGeneratePhashes = getServerBoolean(ServerPreferences.PREF_SCAN_GENERATE_PHASHES),
+                    scanGeneratePreviews = getServerBoolean(ServerPreferences.PREF_SCAN_GENERATE_PREVIEWS),
+                    scanGenerateSprites = getServerBoolean(ServerPreferences.PREF_SCAN_GENERATE_SPRITES),
+                    scanGenerateThumbnails = getServerBoolean(ServerPreferences.PREF_SCAN_GENERATE_THUMBNAILS),
+                    scanGenerateImagePreviews = getServerBoolean(ServerPreferences.PREF_SCAN_GENERATE_IMAGE_PREVIEWS),
+                ),
+            )
+        executeMutation(mutation)
+    }
+
+    suspend fun triggerGenerate() {
+        val mutation =
+            MetadataGenerateMutation(
+                GenerateMetadataInput(
+                    clipPreviews = getServerBoolean(ServerPreferences.PREF_GEN_CLIP_PREVIEWS),
+                    covers = getServerBoolean(ServerPreferences.PREF_GEN_COVERS),
+                    imagePreviews = getServerBoolean(ServerPreferences.PREF_GEN_IMAGE_PREVIEWS),
+                    interactiveHeatmapsSpeeds = getServerBoolean(ServerPreferences.PREF_GEN_INTERACTIVE_HEATMAPS_SPEEDS),
+                    markerImagePreviews = getServerBoolean(ServerPreferences.PREF_GEN_MARKER_IMAGE_PREVIEWS),
+                    markers = getServerBoolean(ServerPreferences.PREF_GEN_MARKERS),
+                    markerScreenshots = getServerBoolean(ServerPreferences.PREF_GEN_MARKER_SCREENSHOTS),
+                    phashes = getServerBoolean(ServerPreferences.PREF_GEN_PHASHES),
+                    previews = getServerBoolean(ServerPreferences.PREF_GEN_PREVIEWS),
+                    sprites = getServerBoolean(ServerPreferences.PREF_GEN_SPRITES),
+                    transcodes = getServerBoolean(ServerPreferences.PREF_GEN_TRANSCODES),
+                ),
+            )
+        executeMutation(mutation)
     }
 
     companion object {
--- app/src/main/java/com/github/damontecres/stashapp/ServerPreferences.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ServerPreferences.kt
@@ -29,11 +29,65 @@ class ServerPreferences(context: Context) {
                     PREF_TRACK_ACTIVITY,
                     (ui.getCaseInsensitive(PREF_TRACK_ACTIVITY) as Boolean?) ?: false,
                 )
+
+                val scan = config.defaults.scan
+                if (scan != null) {
+                    putBoolean(PREF_SCAN_GENERATE_COVERS, scan.scanGenerateCovers)
+                    putBoolean(PREF_SCAN_GENERATE_PREVIEWS, scan.scanGeneratePreviews)
+                    putBoolean(PREF_SCAN_GENERATE_IMAGE_PREVIEWS, scan.scanGenerateImagePreviews)
+                    putBoolean(PREF_SCAN_GENERATE_SPRITES, scan.scanGenerateSprites)
+                    putBoolean(PREF_SCAN_GENERATE_PHASHES, scan.scanGeneratePhashes)
+                    putBoolean(PREF_SCAN_GENERATE_THUMBNAILS, scan.scanGenerateThumbnails)
+                    putBoolean(PREF_SCAN_GENERATE_CLIP_PREVIEWS, scan.scanGenerateClipPreviews)
+                }
+
+                val generate = config.defaults.generate
+                if (generate != null) {
+                    putBoolean(PREF_GEN_CLIP_PREVIEWS, generate.clipPreviews ?: false)
+                    putBoolean(PREF_GEN_COVERS, generate.covers ?: false)
+                    putBoolean(PREF_GEN_IMAGE_PREVIEWS, generate.imagePreviews ?: false)
+                    putBoolean(
+                        PREF_GEN_INTERACTIVE_HEATMAPS_SPEEDS,
+                        generate.interactiveHeatmapsSpeeds ?: false,
+                    )
+                    putBoolean(
+                        PREF_GEN_MARKER_IMAGE_PREVIEWS,
+                        generate.markerImagePreviews ?: false,
+                    )
+                    putBoolean(PREF_GEN_MARKERS, generate.markers ?: false)
+                    putBoolean(PREF_GEN_MARKER_SCREENSHOTS, generate.markerScreenshots ?: false)
+                    putBoolean(PREF_GEN_PHASHES, generate.phashes ?: false)
+                    putBoolean(PREF_GEN_PREVIEWS, generate.previews ?: false)
+                    putBoolean(PREF_GEN_SPRITES, generate.sprites ?: false)
+                    putBoolean(PREF_GEN_TRANSCODES, generate.transcodes ?: false)
+                }
             }
         }
     }
 
     companion object {
         const val PREF_TRACK_ACTIVITY = "trackActivity"
+
+        // Scan default settings
+        const val PREF_SCAN_GENERATE_COVERS = "scanGenerateCovers"
+        const val PREF_SCAN_GENERATE_PREVIEWS = "scanGeneratePreviews"
+        const val PREF_SCAN_GENERATE_IMAGE_PREVIEWS = "scanGenerateImagePreviews"
+        const val PREF_SCAN_GENERATE_SPRITES = "scanGenerateSprites"
+        const val PREF_SCAN_GENERATE_PHASHES = "scanGeneratePhashes"
+        const val PREF_SCAN_GENERATE_THUMBNAILS = "scanGenerateThumbnails"
+        const val PREF_SCAN_GENERATE_CLIP_PREVIEWS = "scanGenerateClipPreviews"
+
+        // Generate default settings
+        const val PREF_GEN_CLIP_PREVIEWS = "clipPreviews"
+        const val PREF_GEN_COVERS = "covers"
+        const val PREF_GEN_IMAGE_PREVIEWS = "imagePreviews"
+        const val PREF_GEN_INTERACTIVE_HEATMAPS_SPEEDS = "interactiveHeatmapsSpeeds"
+        const val PREF_GEN_MARKER_IMAGE_PREVIEWS = "markerImagePreviews"
+        const val PREF_GEN_MARKERS = "markers"
+        const val PREF_GEN_MARKER_SCREENSHOTS = "markerScreenshots"
+        const val PREF_GEN_PHASHES = "phashes"
+        const val PREF_GEN_PREVIEWS = "previews"
+        const val PREF_GEN_SPRITES = "sprites"
+        const val PREF_GEN_TRANSCODES = "transcodes"
     }
 }
