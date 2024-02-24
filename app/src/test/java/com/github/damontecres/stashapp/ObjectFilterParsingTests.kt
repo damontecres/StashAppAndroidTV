551529c46b4357f31b98d0d1f6f6090c9257940d
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-02-24 17:58:17 -0500

    Fix unit tests

--- app/src/test/java/com/github/damontecres/stashapp/ObjectFilterParsingTests.kt
+++ app/src/test/java/com/github/damontecres/stashapp/ObjectFilterParsingTests.kt
@@ -1,13 +1,12 @@
 package com.github.damontecres.stashapp
 
-import android.content.Context
 import com.apollographql.apollo3.api.json.BufferedSourceJsonReader
 import com.apollographql.apollo3.api.parseJsonResponse
 import com.github.damontecres.stashapp.api.FindSavedFilterQuery
-import com.github.damontecres.stashapp.api.ServerInfoQuery
 import com.github.damontecres.stashapp.api.fragment.SavedFilterData
 import com.github.damontecres.stashapp.api.type.FilterMode
 import com.github.damontecres.stashapp.util.FilterParser
+import com.github.damontecres.stashapp.util.Version
 import okio.FileSystem
 import okio.Path.Companion.toPath
 import org.junit.Assert
@@ -15,20 +14,12 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.mock
 
 @RunWith(MockitoJUnitRunner::class)
 class ObjectFilterParsingTests {
-    private val serverInfo =
-        ServerInfoQuery.Data(
-            version = ServerInfoQuery.Version("0.23.0"),
-            stats = ServerInfoQuery.Stats(1),
-        )
-
     @Before
     fun init() {
-        val mockContext = mock<Context> {}
-        FilterParser.initialize(mockContext, serverInfo)
+//        val mockContext = mock<Context> {}
     }
 
     /**
@@ -47,7 +38,7 @@ class ObjectFilterParsingTests {
     fun testSceneFilter() {
         val savedFilterData = getSavedFilterData("scene_savedfilter.json")
         val sceneFilter =
-            FilterParser.instance.convertSceneObjectFilter(savedFilterData.object_filter)
+            FilterParser(Version.V0_24_3).convertSceneObjectFilter(savedFilterData.object_filter)
         Assert.assertNotNull(sceneFilter!!)
         Assert.assertEquals(FilterMode.SCENES, savedFilterData.mode)
         Assert.assertEquals(
@@ -80,7 +71,7 @@ class ObjectFilterParsingTests {
     fun testPerformerFilter() {
         val savedFilterData = getSavedFilterData("performer_savedfilter.json")
         val performerFilter =
-            FilterParser.instance.convertPerformerObjectFilter(savedFilterData.object_filter)
+            FilterParser(Version.V0_24_3).convertPerformerObjectFilter(savedFilterData.object_filter)
         Assert.assertNotNull(performerFilter!!)
         Assert.assertEquals(FilterMode.PERFORMERS, savedFilterData.mode)
 
