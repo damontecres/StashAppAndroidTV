f136fef4766a075f35a73fe08df250df0dc9473f
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-02-25 16:48:04 -0500

    Update movie layout

--- app/src/main/java/com/github/damontecres/stashapp/MovieFragment.kt
+++ app/src/main/java/com/github/damontecres/stashapp/MovieFragment.kt
@@ -4,17 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.github.damontecres.stashapp.data.Movie
+import com.github.damontecres.stashapp.presenters.MoviePresenter
 import com.github.damontecres.stashapp.presenters.StashPresenter
 import com.github.damontecres.stashapp.util.createGlideUrl
 
-class MovieFragment : Fragment(R.layout.performer_view) {
-    private lateinit var mPerformerImage: ImageView
-    private lateinit var mPerformerName: TextView
-    private lateinit var mPerformerDisambiguation: TextView
+class MovieFragment : Fragment(R.layout.movie_view) {
+    private lateinit var frontImage: ImageView
+    private lateinit var backImage: ImageView
+    private lateinit var titleText: TextView
+    private lateinit var table: TableLayout
 
     override fun onViewCreated(
         view: View,
@@ -22,26 +26,69 @@ class MovieFragment : Fragment(R.layout.performer_view) {
     ) {
         super.onViewCreated(view, savedInstanceState)
 
-        mPerformerImage = view.findViewById(R.id.performer_image)
-        mPerformerName = view.findViewById(R.id.performer_name)
-        mPerformerDisambiguation = view.findViewById(R.id.performer_disambiguation)
+        frontImage = view.findViewById(R.id.movie_front_image)
+        backImage = view.findViewById(R.id.movie_back_image)
+        titleText = view.findViewById(R.id.movie_name)
+
+        table = view.findViewById(R.id.movie_table)
 
         val movie = requireActivity().intent.getParcelableExtra<Movie>("movie")
         if (movie != null) {
-            mPerformerName.text = movie.name
-            mPerformerDisambiguation.text = movie.aliases
-
+            titleText.text = movie.name
             if (movie.frontImagePath != null) {
+                configureLayout(frontImage)
                 val url = createGlideUrl(movie.frontImagePath, requireContext())
                 Glide.with(requireActivity())
                     .load(url)
                     .centerCrop()
                     .error(StashPresenter.glideError(requireContext()))
-                    .into(mPerformerImage)
+                    .into(frontImage)
             }
+            if (movie.backImagePath != null) {
+                configureLayout(backImage)
+                val url = createGlideUrl(movie.backImagePath, requireContext())
+                Glide.with(requireActivity())
+                    .load(url)
+                    .centerCrop()
+                    .error(StashPresenter.glideError(requireContext()))
+                    .into(backImage)
+            }
+//            viewLifecycleOwner.lifecycleScope.launch(StashCoroutineExceptionHandler()) {
+//                val queryEngine = QueryEngine(requireContext())
+//
+//            }
         } else {
             val intent = Intent(requireActivity(), MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun configureLayout(view: ImageView) {
+        val lp = view.layoutParams
+        val scale = 1.75
+        lp.width = (MoviePresenter.CARD_WIDTH * scale).toInt()
+        lp.height = (MoviePresenter.CARD_HEIGHT * scale).toInt()
+        view.layoutParams = lp
+    }
+
+    private fun addRow(
+        key: Int,
+        value: String?,
+    ) {
+        if (value.isNullOrBlank()) {
+            return
+        }
+        val keyString = getString(key) + ":"
+
+        val row =
+            requireActivity().layoutInflater.inflate(R.layout.table_row, table, false) as TableRow
+
+        val keyView = row.findViewById<TextView>(R.id.table_row_key)
+        keyView.text = keyString
+
+        val valueView = row.findViewById<TextView>(R.id.table_row_value)
+        valueView.text = value
+
+        table.addView(row)
     }
 }
