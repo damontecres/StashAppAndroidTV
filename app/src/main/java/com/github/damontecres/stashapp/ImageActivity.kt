06cfaf9beae2dc2c6812439b6518dbc9f4ee6aa2
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-03-02 22:27:07 -0500

    Add details to image overlay

--- app/src/main/java/com/github/damontecres/stashapp/ImageActivity.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ImageActivity.kt
@@ -7,6 +7,8 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.isVisible
@@ -21,6 +23,7 @@ import com.bumptech.glide.request.target.Target
 import com.github.damontecres.stashapp.api.fragment.ImageData
 import com.github.damontecres.stashapp.util.QueryEngine
 import com.github.damontecres.stashapp.util.StashCoroutineExceptionHandler
+import com.github.damontecres.stashapp.util.concatIfNotBlank
 import com.github.damontecres.stashapp.util.createGlideUrl
 import kotlinx.coroutines.launch
 
@@ -42,11 +45,11 @@ class ImageActivity : FragmentActivity() {
         if (event.action == KeyEvent.ACTION_DOWN) {
             if (event.keyCode == KeyEvent.KEYCODE_BACK) {
                 if (imageFragment.titleText.isVisible) {
-                    imageFragment.titleText.visibility = View.GONE
+                    imageFragment.hideOverlay()
                     return true
                 }
             } else if (!imageFragment.titleText.isVisible) {
-                imageFragment.titleText.visibility = View.VISIBLE
+                imageFragment.showOverlay()
                 return true
             }
         }
@@ -61,6 +64,7 @@ class ImageActivity : FragmentActivity() {
 
     class ImageFragment : Fragment(R.layout.image_layout) {
         lateinit var titleText: TextView
+        lateinit var table: TableLayout
         lateinit var image: ImageData
 
         override fun onViewCreated(
@@ -69,6 +73,7 @@ class ImageActivity : FragmentActivity() {
         ) {
             titleText = view.findViewById(R.id.image_view_title)
             val mainImage = view.findViewById<ImageView>(R.id.image_view_image)
+            table = view.findViewById(R.id.image_view_table)
             val imageUrl = requireActivity().intent.getStringExtra(INTENT_IMAGE_URL)!!
             val imageId = requireActivity().intent.getStringExtra(INTENT_IMAGE_ID)!!
             Log.v(TAG, "imageId=$imageId")
@@ -77,6 +82,19 @@ class ImageActivity : FragmentActivity() {
                 image = queryEngine.getImage(imageId)!!
                 Log.v(TAG, "image.id=${image.id}")
                 titleText.text = image.title
+
+                addRow(R.string.stashapp_studio, image.studio?.studioData?.name)
+                addRow(R.string.stashapp_date, image.date)
+                addRow(R.string.stashapp_photographer, image.photographer)
+                addRow(R.string.stashapp_details, image.details)
+                addRow(
+                    R.string.stashapp_tags,
+                    concatIfNotBlank(", ", image.tags.map { it.tagData.name }),
+                )
+                addRow(
+                    R.string.stashapp_performers,
+                    concatIfNotBlank(", ", image.performers.map { it.performerData.name }),
+                )
             }
 
             // Images larger than 5mb need disk cache
@@ -112,6 +130,41 @@ class ImageActivity : FragmentActivity() {
                     },
                 )
                 .into(mainImage)
+        }
+
+        fun showOverlay() {
+            titleText.visibility = View.VISIBLE
+            table.visibility = View.VISIBLE
+        }
+
+        fun hideOverlay() {
+            titleText.visibility = View.GONE
+            table.visibility = View.GONE
+        }
+
+        private fun addRow(
+            key: Int,
+            value: String?,
+        ) {
+            if (value.isNullOrBlank()) {
+                return
+            }
+            val keyString = getString(key) + ":"
+
+            val row =
+                requireActivity().layoutInflater.inflate(
+                    R.layout.table_row,
+                    table,
+                    false,
+                ) as TableRow
+
+            val keyView = row.findViewById<TextView>(R.id.table_row_key)
+            keyView.text = keyString
+
+            val valueView = row.findViewById<TextView>(R.id.table_row_value)
+            valueView.text = value
+
+            table.addView(row)
         }
     }
 }
