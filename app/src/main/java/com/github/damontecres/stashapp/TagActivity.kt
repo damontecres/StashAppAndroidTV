c55cf59e9e31a9c701ff43880d9c4b49c5498505
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-03-04 20:32:16 -0500

    Add galleries & images to tag page

--- app/src/main/java/com/github/damontecres/stashapp/TagActivity.kt
+++ app/src/main/java/com/github/damontecres/stashapp/TagActivity.kt
@@ -5,18 +5,25 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.leanback.tab.LeanbackTabLayout
 import androidx.leanback.tab.LeanbackViewPager
 import com.apollographql.apollo3.api.Optional
 import com.github.damontecres.stashapp.api.type.CriterionModifier
+import com.github.damontecres.stashapp.api.type.GalleryFilterType
 import com.github.damontecres.stashapp.api.type.HierarchicalMultiCriterionInput
+import com.github.damontecres.stashapp.api.type.ImageFilterType
 import com.github.damontecres.stashapp.api.type.PerformerFilterType
 import com.github.damontecres.stashapp.api.type.SceneFilterType
 import com.github.damontecres.stashapp.api.type.SceneMarkerFilterType
+import com.github.damontecres.stashapp.data.DataType
+import com.github.damontecres.stashapp.suppliers.GalleryDataSupplier
+import com.github.damontecres.stashapp.suppliers.ImageDataSupplier
 import com.github.damontecres.stashapp.suppliers.MarkerDataSupplier
 import com.github.damontecres.stashapp.suppliers.PerformerDataSupplier
 import com.github.damontecres.stashapp.suppliers.SceneDataSupplier
+import com.github.damontecres.stashapp.util.GalleryComparator
+import com.github.damontecres.stashapp.util.ImageComparator
+import com.github.damontecres.stashapp.util.ListFragmentPagerAdapter
 import com.github.damontecres.stashapp.util.MarkerComparator
 import com.github.damontecres.stashapp.util.PerformerComparator
 import com.github.damontecres.stashapp.util.SceneComparator
@@ -35,27 +42,23 @@ class TagActivity : FragmentActivity() {
             val viewPager = findViewById<LeanbackViewPager>(R.id.view_pager)
             val tabLayout = findViewById<LeanbackTabLayout>(R.id.tab_layout)
 
-            val tagAdapter = TabPageAdapter(tagId, supportFragmentManager)
+            val tabs =
+                listOf(
+                    getString(DataType.SCENE.pluralStringId),
+                    getString(DataType.GALLERY.pluralStringId),
+                    getString(DataType.IMAGE.pluralStringId),
+                    getString(DataType.MARKER.pluralStringId),
+                    getString(DataType.PERFORMER.pluralStringId),
+                )
+
+            val tagAdapter = TabPageAdapter(tabs, tagId, supportFragmentManager)
             viewPager.adapter = tagAdapter
             tabLayout.setupWithViewPager(viewPager)
         }
     }
 
-    class TabPageAdapter(private val tagId: String, fm: FragmentManager) :
-        FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-        override fun getCount(): Int {
-            return 3
-        }
-
-        override fun getPageTitle(position: Int): CharSequence? {
-            return when (position) {
-                0 -> "Scenes"
-                1 -> "Markers"
-                2 -> "Performers"
-                else -> throw IllegalStateException()
-            }
-        }
-
+    class TabPageAdapter(tabs: List<String>, private val tagId: String, fm: FragmentManager) :
+        ListFragmentPagerAdapter(tabs, fm) {
         override fun getItem(position: Int): Fragment {
             return if (position == 0) {
                 StashGridFragment(
@@ -74,6 +77,36 @@ class TagActivity : FragmentActivity() {
                 )
             } else if (position == 1) {
                 StashGridFragment(
+                    GalleryComparator,
+                    GalleryDataSupplier(
+                        GalleryFilterType(
+                            tags =
+                                Optional.present(
+                                    HierarchicalMultiCriterionInput(
+                                        value = Optional.present(listOf(tagId)),
+                                        modifier = CriterionModifier.INCLUDES,
+                                    ),
+                                ),
+                        ),
+                    ),
+                )
+            } else if (position == 2) {
+                StashGridFragment(
+                    ImageComparator,
+                    ImageDataSupplier(
+                        ImageFilterType(
+                            tags =
+                                Optional.present(
+                                    HierarchicalMultiCriterionInput(
+                                        value = Optional.present(listOf(tagId)),
+                                        modifier = CriterionModifier.INCLUDES,
+                                    ),
+                                ),
+                        ),
+                    ),
+                )
+            } else if (position == 3) {
+                StashGridFragment(
                     MarkerComparator,
                     MarkerDataSupplier(
                         SceneMarkerFilterType(
@@ -87,7 +120,7 @@ class TagActivity : FragmentActivity() {
                         ),
                     ),
                 )
-            } else if (position == 2) {
+            } else if (position == 4) {
                 StashGridFragment(
                     PerformerComparator,
                     PerformerDataSupplier(
