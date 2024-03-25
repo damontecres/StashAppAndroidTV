04c5e5be43f9b4400bf589f59e7c6deb7e82dab9
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-03-24 20:37:58 -0400

    Measure popup item widths

--- app/src/main/java/com/github/damontecres/stashapp/presenters/PopupOnLongClickListener.kt
+++ app/src/main/java/com/github/damontecres/stashapp/presenters/PopupOnLongClickListener.kt
@@ -1,9 +1,13 @@
 package com.github.damontecres.stashapp.presenters
 
+import android.content.Context
 import android.view.View
+import android.view.View.MeasureSpec
 import android.view.View.OnLongClickListener
+import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.FrameLayout
 import androidx.appcompat.widget.ListPopupWindow
 import com.github.damontecres.stashapp.R
 
@@ -22,17 +26,23 @@ class PopupOnLongClickListener(
                 null,
                 android.R.attr.listPopupWindowStyle,
             )
-        listPopUp.inputMethodMode = ListPopupWindow.INPUT_METHOD_NEEDED
-        listPopUp.anchorView = view
-        listPopUp.width = popUpWidth
-        listPopUp.isModal = true
-
         val adapter =
             ArrayAdapter(
                 view.context,
                 R.layout.popup_item,
                 popupOptions,
             )
+
+        listPopUp.inputMethodMode = ListPopupWindow.INPUT_METHOD_NEEDED
+        listPopUp.anchorView = view
+        listPopUp.width =
+            if (popUpWidth == ListPopupWindow.WRAP_CONTENT) {
+                getMaxWidth(view.context, adapter)
+            } else {
+                popUpWidth
+            }
+        listPopUp.isModal = true
+
         listPopUp.setAdapter(adapter)
 
         listPopUp.setOnItemClickListener { parent: AdapterView<*>, v: View, position: Int, id: Long ->
@@ -43,5 +53,30 @@ class PopupOnLongClickListener(
         listPopUp.show()
 
         return true
+    }
+
+    private fun getMaxWidth(
+        context: Context,
+        adapter: ArrayAdapter<String>,
+    ): Int {
+        if (adapter.viewTypeCount != 1) {
+            throw IllegalStateException("Adapter creates more than 1 type of view")
+        }
+
+        val tempParent = FrameLayout(context)
+        var maxWidth = 0
+        var itemView: View? = null
+        val measureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+
+        for (i in 0 until adapter.count) {
+            if (adapter.getItemViewType(i) != Adapter.IGNORE_ITEM_VIEW_TYPE) {
+                itemView = adapter.getView(i, itemView, tempParent)
+                itemView.measure(measureSpec, measureSpec)
+                if (itemView.measuredWidth > maxWidth) {
+                    maxWidth = itemView.measuredWidth
+                }
+            }
+        }
+        return maxWidth
     }
 }
