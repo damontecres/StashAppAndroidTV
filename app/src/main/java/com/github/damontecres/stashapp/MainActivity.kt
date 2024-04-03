af655ba7622769723cf6f0616f098e50e15976c4
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-04-03 18:45:49 -0400

    Add preference to disable update checks

--- app/src/main/java/com/github/damontecres/stashapp/MainActivity.kt
+++ app/src/main/java/com/github/damontecres/stashapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.damontecres.stashapp
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.preference.PreferenceManager
 import com.github.damontecres.stashapp.util.StashCoroutineExceptionHandler
 import com.github.damontecres.stashapp.util.UpdateChecker
 import kotlinx.coroutines.launch
@@ -20,8 +21,14 @@ class MainActivity : FragmentActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_browse_fragment, fragment)
                 .commitNow()
-            lifecycleScope.launch(StashCoroutineExceptionHandler()) {
-                UpdateChecker.checkForUpdate(this@MainActivity, false)
+
+            val checkForUpdates =
+                PreferenceManager.getDefaultSharedPreferences(this)
+                    .getBoolean("autoCheckForUpdates", true)
+            if (checkForUpdates) {
+                lifecycleScope.launch(StashCoroutineExceptionHandler()) {
+                    UpdateChecker.checkForUpdate(this@MainActivity, false)
+                }
             }
         }
     }
