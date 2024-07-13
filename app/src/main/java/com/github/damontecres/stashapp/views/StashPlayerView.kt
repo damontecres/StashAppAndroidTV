cd6f8f9958aa697461b256c2fcb3a003793259b8
damontecres <154766448+damontecres@users.noreply.github.com>
2024-07-13 12:42:27 -0400

    Don't show controls when resuming playback with remote's play button (#324)
    If a video is paused and then resumed by pressing play (or play_pause)
    on a remote, then resume playback without showing the playback
    controller overlay.
    
    This is more like other players and is less distracting.

--- app/src/main/java/com/github/damontecres/stashapp/views/StashPlayerView.kt
+++ app/src/main/java/com/github/damontecres/stashapp/views/StashPlayerView.kt
@@ -5,7 +5,9 @@ import android.util.AttributeSet
 import android.view.KeyEvent
 import androidx.annotation.OptIn
 import androidx.fragment.app.findFragment
+import androidx.media3.common.Player.Listener
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
 import androidx.media3.ui.PlayerView
 import androidx.preference.PreferenceManager
 import com.github.damontecres.stashapp.PlaybackExoFragment
@@ -23,7 +25,45 @@ class StashPlayerView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
         val keyCode = event.keyCode
         val fragment = findFragment<PlaybackExoFragment>()
-        if (player != null && !fragment.isControllerVisible &&
+        if (player != null &&
+            (keyCode == KeyEvent.KEYCODE_MEDIA_PLAY || keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)
+        ) {
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                val isPaused = !player!!.isPlaying
+                if (isPaused) {
+                    // If paused and user presses play, resume playback without showing controls
+                    if (!fragment.isControllerVisible) {
+                        // If the controller is already visible, don't change its behavior
+                        useController = false
+                        player!!.addListener(
+                            object : Listener {
+                                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                                    if (isPlaying) {
+                                        useController = true
+                                        player!!.removeListener(this)
+                                    }
+                                }
+                            },
+                        )
+                    }
+                    when (keyCode) {
+                        KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE ->
+                            Util.handlePlayPauseButtonAction(
+                                player,
+                                true,
+                            )
+
+                        KeyEvent.KEYCODE_MEDIA_PLAY -> Util.handlePlayButtonAction(player)
+                    }
+                    return true
+                } else {
+                    // Not paused, so allow normal handling
+                    return super.dispatchKeyEvent(event)
+                }
+            } else {
+                return true
+            }
+        } else if (player != null && !fragment.isControllerVisible &&
             (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT || keyCode == KeyEvent.KEYCODE_DPAD_LEFT)
         ) {
             if (event.action == KeyEvent.ACTION_DOWN) {
