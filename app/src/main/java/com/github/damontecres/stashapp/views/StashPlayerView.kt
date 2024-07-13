f6b3006b4f8e3c048a098d9769f79ec11e7b625c
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-07-13 11:22:58 -0400

    When resuming playback don't show controls

--- app/src/main/java/com/github/damontecres/stashapp/views/StashPlayerView.kt
+++ app/src/main/java/com/github/damontecres/stashapp/views/StashPlayerView.kt
@@ -2,9 +2,11 @@ package com.github.damontecres.stashapp.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.KeyEvent
 import androidx.annotation.OptIn
 import androidx.fragment.app.findFragment
+import androidx.media3.common.Player.Listener
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.ui.PlayerView
 import androidx.preference.PreferenceManager
@@ -23,7 +25,39 @@ class StashPlayerView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
     override fun dispatchKeyEvent(event: KeyEvent): Boolean {
         val keyCode = event.keyCode
         val fragment = findFragment<PlaybackExoFragment>()
-        if (player != null && !fragment.isControllerVisible &&
+        if (player != null &&
+            (keyCode == KeyEvent.KEYCODE_MEDIA_PLAY || keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)
+        ) {
+            val isPaused = !player!!.isPlaying
+            if (event.action == KeyEvent.ACTION_DOWN && isPaused) {
+                // If paused and user presses play, resume playback without showing controls
+                Log.v(TAG, "Resuming")
+                if (!fragment.isControllerVisible) {
+                    // If the controller is visible, we don't want to change its behavior
+                    controllerAutoShow = false
+                    player!!.addListener(
+                        object : Listener {
+                            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                                Log.v(
+                                    TAG,
+                                    "onIsPlayingChanged: controllerAutoShow=$controllerAutoShow, useController=$useController",
+                                )
+                                if (isPlaying) {
+                                    controllerAutoShow = true
+                                    player!!.removeListener(this)
+                                }
+                            }
+                        },
+                    )
+                }
+                player!!.play()
+                return true
+            } else if (event.action == KeyEvent.ACTION_DOWN && !isPaused) {
+                return super.dispatchKeyEvent(event)
+            } else {
+                return true
+            }
+        } else if (player != null && !fragment.isControllerVisible &&
             (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT || keyCode == KeyEvent.KEYCODE_DPAD_LEFT)
         ) {
             if (event.action == KeyEvent.ACTION_DOWN) {
