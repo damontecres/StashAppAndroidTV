d58d96d3a38799f46c144311b2f03df336a94152
damontecres <154766448+damontecres@users.noreply.github.com>
2024-07-17 21:43:30 -0400

    Image & Marker scrolling perserve sort changes (#330)
    Fixes #326
    
    Changes to the sort by/direction are saved. Scrolling through images and
    playing all markers will now reflect those changes.

--- app/src/main/java/com/github/damontecres/stashapp/data/StashSavedFilter.kt
+++ app/src/main/java/com/github/damontecres/stashapp/data/StashSavedFilter.kt
@@ -1,6 +1,7 @@
 package com.github.damontecres.stashapp.data
 
 import android.os.Parcelable
+import com.github.damontecres.stashapp.api.fragment.SavedFilterData
 import com.github.damontecres.stashapp.api.type.FilterMode
 import kotlinx.parcelize.Parcelize
 
@@ -8,8 +9,16 @@ import kotlinx.parcelize.Parcelize
 data class StashSavedFilter(
     val savedFilterId: String,
     val mode: FilterMode,
-    val sortBy: String? = null,
+    override val sortBy: String? = null,
+    override val direction: String? = null,
 ) : Parcelable, StashFilter {
+    constructor(savedFilterData: SavedFilterData) : this(
+        savedFilterData.id,
+        savedFilterData.mode,
+        savedFilterData.find_filter?.sort,
+        savedFilterData.find_filter?.direction?.toString(),
+    )
+
     override val filterType: FilterType
         get() = FilterType.SAVED_FILTER
     override val dataType: DataType
