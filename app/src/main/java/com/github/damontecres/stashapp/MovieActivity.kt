damontecres/StashAppAndroidTV

commit cace0784eb86e4ee046be2425190ae0d469b6bf1

Add sort button to grids (#360)
### UI/UX

Adds a sort by button to all filter grids including ones in tabs. In
most cases, the sort button will disappear after scrolling down a bit.

Filter grids won't crash the app when returning to them after they were
removed from memory.


### Dev

1. Add parcel implementations for object filters
2. Rewrite `StashGridFragment` so it is compatible with #328
3. Add `SortButtonManager` to easily manager the sort button

app/src/main/java/com/github/damontecres/stashapp/MovieActivity.kt
@@ -2,17 +2,16 @@ package com.github.damontecres.stashapp
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import androidx.leanback.widget.ObjectAdapter
 import androidx.preference.PreferenceManager
 import com.apollographql.apollo3.api.Optional
 import com.github.damontecres.stashapp.api.type.CriterionModifier
-import com.github.damontecres.stashapp.api.type.FindFilterType
 import com.github.damontecres.stashapp.api.type.MultiCriterionInput
 import com.github.damontecres.stashapp.api.type.SceneFilterType
 import com.github.damontecres.stashapp.api.type.SortDirectionEnum
+import com.github.damontecres.stashapp.data.DataType
 import com.github.damontecres.stashapp.data.Movie
-import com.github.damontecres.stashapp.suppliers.SceneDataSupplier
-import com.github.damontecres.stashapp.util.SceneComparator
+import com.github.damontecres.stashapp.data.SortAndDirection
+import com.github.damontecres.stashapp.data.StashFindFilter
 import com.github.damontecres.stashapp.util.getInt
 
 class MovieActivity : FragmentActivity() {
@@ -29,12 +28,10 @@ class MovieActivity : FragmentActivity() {
 
             val sceneFragment =
                 StashGridFragment(
-                    SceneComparator,
-                    SceneDataSupplier(
-                        FindFilterType(
-                            sort = Optional.present("movie_scene_number"),
-                            direction = Optional.present(SortDirectionEnum.ASC),
-                        ),
+                    dataType = DataType.SCENE,
+                    findFilter =
+                        StashFindFilter(SortAndDirection("movie_scene_number", SortDirectionEnum.ASC)),
+                    objectFilter =
                         SceneFilterType(
                             movies =
                                 Optional.present(
@@ -44,22 +41,15 @@ class MovieActivity : FragmentActivity() {
                                     ),
                                 ),
                         ),
-                    ),
-                    columns,
+                    cardSize = columns,
                 )
+            sceneFragment.sortButtonEnabled = true
+            sceneFragment.requestFocus = true
 
             supportFragmentManager.beginTransaction()
                 .replace(R.id.movie_fragment, MovieFragment())
                 .replace(R.id.movie_list_fragment, sceneFragment)
                 .commitNow()
-            sceneFragment.pagingAdapter.registerObserver(
-                object : ObjectAdapter.DataObserver() {
-                    override fun onChanged() {
-                        sceneFragment.view!!.requestFocus()
-                        sceneFragment.pagingAdapter.unregisterObserver(this)
-                    }
-                },
-            )
         }
     }
 }

commit d0241763a1c7e8dd1dffda62a3548da3937c0405

Fixes a few UI issues (#373)
1. Focus indicator for rating on gallery page
2. Show more columns per row for some data types (a regression from
#360)
3. Don't hide the playback controls when opening the playback options
menu

app/src/main/java/com/github/damontecres/stashapp/MovieActivity.kt
@@ -41,7 +41,7 @@ class MovieActivity : FragmentActivity() {
                                     ),
                                 ),
                         ),
-                    cardSize = columns,
+                    columns = columns,
                 )
             sceneFragment.sortButtonEnabled = true
             sceneFragment.requestFocus = true

app/src/main/java/com/github/damontecres/stashapp/StashGridFragment.kt
@@ -28,6 +28,7 @@ import com.apollographql.apollo3.api.Query
 import com.github.damontecres.stashapp.data.DataType
 import com.github.damontecres.stashapp.data.SortAndDirection
 import com.github.damontecres.stashapp.data.StashFindFilter
+import com.github.damontecres.stashapp.presenters.ScenePresenter
 import com.github.damontecres.stashapp.presenters.StashPresenter
 import com.github.damontecres.stashapp.suppliers.DataSupplierFactory
 import com.github.damontecres.stashapp.suppliers.FilterArgs
@@ -69,7 +70,7 @@ class StashGridFragment() : Fragment() {
     private var mSelectedPosition = -1
     private var titleTransitionHelper: TitleTransitionHelper? = null
     private var sortButtonTransitionHelper: TitleTransitionHelper? = null
-    private var cardSize: Int? = null
+    private var columns: Int? = null
     private var scrollToNextPage = false
 
     // Modifiable properties
@@ -157,12 +158,12 @@ class StashGridFragment() : Fragment() {
 
     constructor(
         filterArgs: FilterArgs,
-        cardSize: Int? = null,
+        columns: Int? = null,
         scrollToNextPage: Boolean = false,
     ) : this() {
         this._filterArgs =
             filterArgs.ensureParsed(FilterParser(StashServer.getCurrentServerVersion()))
-        this.cardSize = cardSize
+        this.columns = columns
         this._currentSortAndDirection = _filterArgs.sortAndDirection
         this.scrollToNextPage = scrollToNextPage
     }
@@ -171,9 +172,9 @@ class StashGridFragment() : Fragment() {
         dataType: DataType,
         findFilter: StashFindFilter? = null,
         objectFilter: Any? = null,
-        cardSize: Int? = null,
+        columns: Int? = null,
         scrollToNextPage: Boolean = false,
-    ) : this(FilterArgs(dataType, null, findFilter, objectFilter), cardSize, scrollToNextPage)
+    ) : this(FilterArgs(dataType, null, findFilter, objectFilter), columns, scrollToNextPage)
 
     @SuppressLint("SetTextI18n")
     private fun gridOnItemSelected(position: Int) {
@@ -205,11 +206,11 @@ class StashGridFragment() : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val cardSize =
+        val columns =
             if (savedInstanceState == null) {
-                this.cardSize
+                this.columns
             } else {
-                val temp = savedInstanceState.getInt("cardSize")
+                val temp = savedInstanceState.getInt("columns")
                 if (temp > 0) {
                     temp
                 } else {
@@ -217,12 +218,18 @@ class StashGridFragment() : Fragment() {
                 }
             }
 
-        val gridPresenter = StashGridPresenter()
-        val columns =
-            cardSize ?: PreferenceManager.getDefaultSharedPreferences(requireContext())
-                .getInt("cardSize", requireContext().getString(R.string.card_size_default))
+        val calculatedColumns =
+            if (columns != null) {
+                columns
+            } else {
+                val cardSize =
+                    PreferenceManager.getDefaultSharedPreferences(requireContext())
+                        .getInt("cardSize", requireContext().getString(R.string.card_size_default))
+                (cardSize * (ScenePresenter.CARD_WIDTH.toDouble() / dataType.defaultCardWidth)).toInt()
+            }
 
-        gridPresenter.numberOfColumns = columns
+        val gridPresenter = StashGridPresenter()
+        gridPresenter.numberOfColumns = calculatedColumns
         setGridPresenter(gridPresenter)
 
         if (savedInstanceState != null) {
@@ -305,7 +312,7 @@ class StashGridFragment() : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putInt("mSelectedPosition", mSelectedPosition)
-        cardSize?.let { outState.putInt("cardSize", it) }
+        columns?.let { outState.putInt("columns", it) }
         outState.putParcelable("_filterArgs", _filterArgs.with(currentSortAndDirection))
         outState.putString("name", name)
         outState.putBoolean("sortButtonEnabled", sortButtonEnabled)

app/src/main/java/com/github/damontecres/stashapp/playback/PlaybackSceneFragment.kt
@@ -34,8 +34,8 @@ import com.github.damontecres.stashapp.util.ServerPreferences
 import com.github.damontecres.stashapp.util.StashCoroutineExceptionHandler
 import com.github.damontecres.stashapp.util.StashServer
 import com.github.damontecres.stashapp.util.toMilliseconds
+import com.github.damontecres.stashapp.views.ListPopupWindowBuilder
 import com.github.damontecres.stashapp.views.durationToString
-import com.github.damontecres.stashapp.views.showSimpleListPopupWindow
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -227,6 +227,7 @@ class PlaybackSceneFragment : PlaybackFragment() {
         )
 
         moreOptionsButton.setOnClickListener {
+            val previousControllerShowTimeoutMs = videoView.controllerShowTimeoutMs
             val debugToggleText =
                 if (debugView.isVisible) "Hide transcode info" else "Show transcode info"
             val applyFiltersText =
@@ -239,7 +240,7 @@ class PlaybackSceneFragment : PlaybackFragment() {
                 } else {
                     null
                 }
-            showSimpleListPopupWindow(
+            ListPopupWindowBuilder(
                 moreOptionsButton,
                 listOfNotNull(
                     "Create Marker",
@@ -273,7 +274,14 @@ class PlaybackSceneFragment : PlaybackFragment() {
                         )
                         .commitNow()
                 }
-            }
+            }.onShowListener {
+                // Prevent the controller from hiding
+                videoView.controllerShowTimeoutMs = -1
+            }.onDismissListener {
+                // Restore previous controller timeout
+                videoView.controllerShowTimeoutMs = previousControllerShowTimeoutMs
+            }.build()
+                .show()
         }
     }
 

app/src/main/java/com/github/damontecres/stashapp/views/SimpleListPopupWindow.kt
@@ -3,6 +3,7 @@ package com.github.damontecres.stashapp.views
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.PopupWindow
 import androidx.appcompat.widget.ListPopupWindow
 import com.github.damontecres.stashapp.R
 import com.github.damontecres.stashapp.util.getMaxMeasuredWidth
@@ -15,30 +16,60 @@ fun showSimpleListPopupWindow(
     options: List<String>,
     callback: (position: Int) -> Unit,
 ) {
-    val context = anchorView.context
-    val listPopUp =
-        ListPopupWindow(
-            context,
-            null,
-            android.R.attr.listPopupWindowStyle,
-        )
-    val adapter =
-        ArrayAdapter(
-            context,
-            R.layout.popup_item,
-            options,
-        )
-    listPopUp.setAdapter(adapter)
-    listPopUp.inputMethodMode = ListPopupWindow.INPUT_METHOD_NEEDED
-    listPopUp.anchorView = anchorView
+    ListPopupWindowBuilder(anchorView, options, callback).build().show()
+}
 
-    listPopUp.width = getMaxMeasuredWidth(context, adapter)
-    listPopUp.isModal = true
+class ListPopupWindowBuilder(
+    private val anchorView: View,
+    private val options: List<String>,
+    private val itemClickListener: (position: Int) -> Unit,
+) {
+    private var showListener: (() -> Unit)? = null
+    private var dismissListener: PopupWindow.OnDismissListener? = null
 
-    listPopUp.setOnItemClickListener { _: AdapterView<*>, _: View, position: Int, _: Long ->
-        listPopUp.dismiss()
-        callback(position)
+    fun onShowListener(callback: (() -> Unit)): ListPopupWindowBuilder {
+        this.showListener = callback
+        return this
     }
-    listPopUp.show()
-    listPopUp.listView?.requestFocus()
+
+    fun onDismissListener(callback: PopupWindow.OnDismissListener): ListPopupWindowBuilder {
+        this.dismissListener = callback
+        return this
+    }
+
+    fun build(): ListPopupWindow {
+        val context = anchorView.context
+        val listPopUp =
+            object : ListPopupWindow(
+                context,
+                null,
+                android.R.attr.listPopupWindowStyle,
+            ) {
+                override fun show() {
+                    super.show()
+                    showListener?.invoke()
+                    listView?.requestFocus()
+                }
+            }
+
+        listPopUp.setOnDismissListener(dismissListener)
+        val adapter =
+            ArrayAdapter(
+                context,
+                R.layout.popup_item,
+                options,
+            )
+        listPopUp.setAdapter(adapter)
+        listPopUp.inputMethodMode = ListPopupWindow.INPUT_METHOD_NEEDED
+        listPopUp.anchorView = anchorView
+
+        listPopUp.width = getMaxMeasuredWidth(context, adapter)
+        listPopUp.isModal = true
+
+        listPopUp.setOnItemClickListener { _: AdapterView<*>, _: View, position: Int, _: Long ->
+            listPopUp.dismiss()
+            itemClickListener(position)
+        }
+        return listPopUp
+    }
 }

app/src/main/res/drawable/button_selector_default_bg.xml
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<selector xmlns:android="http://schemas.android.com/apk/res/android">
+    <item android:state_pressed="true">
+        <ripple android:color="@color/selected_background"/>
+    </item>
+    <item android:state_focused="true" android:drawable="@color/selected_background" />
+    <item android:drawable="@color/default_background"/>
+</selector>

app/src/main/res/layout/gallery_view.xml
@@ -34,6 +34,7 @@
                 android:id="@+id/gallery_rating_bar"
                 android:layout_width="wrap_content"
                 android:layout_height="wrap_content"
+                android:background="@drawable/button_selector_default_bg"
                 app:layout_constraintStart_toStartOf="parent"
                 app:layout_constraintEnd_toEndOf="parent"
                 app:layout_constraintTop_toTopOf="parent" />