0eb65af29eb181510c911e08ff3efbb172ce9d35
damontecres <154766448+damontecres@users.noreply.github.com>
2024-09-07 19:08:38 -0400

    Add ability to apply video filters during playlist playback (#405)
    Closes #384
    
    Video effects can be applied to all scene playbacks including scene
    markers and playlists.
    
    There's also some updates to back button handling.

--- app/src/main/java/com/github/damontecres/stashapp/playback/PlaylistMarkersFragment.kt
+++ app/src/main/java/com/github/damontecres/stashapp/playback/PlaylistMarkersFragment.kt
@@ -1,18 +1,14 @@
 package com.github.damontecres.stashapp.playback
 
 import android.os.Bundle
-import android.view.View
 import androidx.annotation.OptIn
-import androidx.core.view.isVisible
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import com.github.damontecres.stashapp.api.CountMarkersQuery
 import com.github.damontecres.stashapp.api.FindMarkersQuery
 import com.github.damontecres.stashapp.api.fragment.MarkerData
+import com.github.damontecres.stashapp.data.DataType
 import com.github.damontecres.stashapp.data.Scene
-import com.github.damontecres.stashapp.util.animateToInvisible
-import com.github.damontecres.stashapp.util.animateToVisible
-import com.github.damontecres.stashapp.views.showSimpleListPopupWindow
 import kotlin.properties.Delegates
 
 /**
@@ -22,6 +18,9 @@ import kotlin.properties.Delegates
 class PlaylistMarkersFragment :
     PlaylistFragment<FindMarkersQuery.Data, MarkerData, CountMarkersQuery.Data>() {
     private var duration by Delegates.notNull<Long>()
+
+    override val optionsButtonOptions: OptionsButtonOptions
+        get() = OptionsButtonOptions(DataType.MARKER, true)
 
     override val previewsEnabled: Boolean
         get() = false
@@ -49,33 +48,6 @@ class PlaylistMarkersFragment :
         // Override the skip forward/back since many users will have default seeking values larger than the duration
         super.skipForwardOverride = duration / 4
         super.skipBackOverride = duration / 4
-    }
-
-    @OptIn(UnstableApi::class)
-    override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?,
-    ) {
-        super.onViewCreated(view, savedInstanceState)
-
-        moreOptionsButton.setOnClickListener {
-            val debugToggleText =
-                if (debugView.isVisible) "Hide transcode info" else "Show transcode info"
-            showSimpleListPopupWindow(
-                moreOptionsButton,
-                listOf("Show playlist", debugToggleText),
-            ) { position ->
-                if (position == 0) {
-                    showPlaylist()
-                } else if (position == 1) {
-                    if (debugView.isVisible) {
-                        debugView.animateToInvisible(View.GONE)
-                    } else {
-                        debugView.animateToVisible()
-                    }
-                }
-            }
-        }
     }
 
     companion object {
