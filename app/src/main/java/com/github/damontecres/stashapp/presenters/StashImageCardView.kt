damontecres/StashAppAndroidTV

commit a64f3f735449290c633a27c7af0c71ed38bbfa23

Add a configurable delay before playing card previews (#440)
Closes #438 

Adds a 1 second delay before hiding the overlay and playing a video
preview on a card.

The delay can be configured in advanced UI settings to be 0 to 5
seconds.

The previous behavior can be restored by setting the value to zero.

app/src/main/java/com/github/damontecres/stashapp/presenters/StashImageCardView.kt
@@ -19,6 +19,8 @@ import android.widget.TextView
 import android.widget.ViewSwitcher
 import androidx.core.content.ContextCompat
 import androidx.leanback.widget.ImageCardView
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.lifecycle.lifecycleScope
 import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
@@ -33,6 +35,7 @@ import com.github.damontecres.stashapp.R
 import com.github.damontecres.stashapp.StashApplication
 import com.github.damontecres.stashapp.StashExoPlayer
 import com.github.damontecres.stashapp.data.DataType
+import com.github.damontecres.stashapp.util.StashCoroutineExceptionHandler
 import com.github.damontecres.stashapp.util.StashGlide
 import com.github.damontecres.stashapp.util.StashServer
 import com.github.damontecres.stashapp.util.animateToInvisible
@@ -42,6 +45,11 @@ import com.github.damontecres.stashapp.util.getInt
 import com.github.damontecres.stashapp.util.isNotNullOrBlank
 import com.github.damontecres.stashapp.views.FontSpan
 import com.github.damontecres.stashapp.views.getRatingAsDecimalString
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.EnumMap
 
 class StashImageCardView(context: Context) : ImageCardView(context) {
@@ -77,7 +85,6 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
 
     private var videoView: PlayerView? = null
     val mainView: ViewSwitcher = findViewById(R.id.main_view)
-    var hideOverlayOnSelection = true
 
     private val iconTextView: TextView
 
@@ -91,6 +98,14 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
     private val playVideoPreviews =
         PreferenceManager.getDefaultSharedPreferences(context)
             .getBoolean("playVideoPreviews", true)
+
+    private val videoDelay =
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .getInt(
+                context.getString(R.string.pref_key_ui_card_overlay_delay),
+                context.resources.getInteger(R.integer.pref_key_ui_card_overlay_delay_default),
+            )
+            .toLong()
 
     private val listener =
         object : Player.Listener {
@@ -106,6 +121,8 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
     private val textSize =
         context.resources.getDimension(androidx.leanback.R.dimen.lb_basic_card_content_text_size)
     private val sweat = ContextCompat.getDrawable(context, R.drawable.sweat_drops)!!
+
+    private var delayJob: Job? = null
 
     init {
         mainImageView.visibility = View.VISIBLE
@@ -133,11 +150,12 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
     }
 
     override fun setSelected(selected: Boolean) {
+        delayJob?.cancel()
         if (playVideoPreviews && videoUrl.isNotNullOrBlank()) {
             if (selected) {
                 initPlayer()
                 videoView?.player?.seekToDefaultPosition()
-                videoView?.player?.playWhenReady = true
+//                videoView?.player?.playWhenReady = true
             } else {
                 showImage()
                 StashExoPlayer.removeListeners()
@@ -145,10 +163,20 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
                 videoView?.player = null
             }
         }
-        if (selected && hideOverlayOnSelection) {
-            cardOverlay.clearAnimation()
-            cardOverlay.animateToInvisible(durationMs = animateTime)
-        } else if (hideOverlayOnSelection) {
+        if (selected) {
+            if (videoDelay > 0) {
+                delayJob =
+                    findViewTreeLifecycleOwner()!!.lifecycleScope.launch(Dispatchers.IO + StashCoroutineExceptionHandler()) {
+                        delay(videoDelay)
+                        withContext(Dispatchers.Main) {
+                            hideOverlayAndPlayVideo()
+                        }
+                    }
+            } else {
+                // No delay, so do it immediately
+                hideOverlayAndPlayVideo()
+            }
+        } else {
             cardOverlay.clearAnimation()
             cardOverlay.animateToVisible(animateTime)
         }
@@ -212,7 +240,7 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
         }
         player.prepare()
         player.repeatMode = Player.REPEAT_MODE_ONE
-        player.playWhenReady = true
+        player.playWhenReady = videoDelay <= 0
     }
 
     fun setUpExtraRow(
@@ -278,6 +306,12 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
         // Reset the background back for images so transparent ones show through
         mainView.setBackgroundColor(resources.getColor(R.color.default_card_background, null))
         mainView.displayedChild = 0
+    }
+
+    private fun hideOverlayAndPlayVideo() {
+        videoView?.player?.play()
+        cardOverlay.clearAnimation()
+        cardOverlay.animateToInvisible(durationMs = animateTime)
     }
 
     fun getTextOverlay(position: OverlayPosition): TextView {

app/src/main/java/com/github/damontecres/stashapp/SettingsUiFragment.kt
@@ -2,6 +2,8 @@ package com.github.damontecres.stashapp
 
 import android.os.Bundle
 import androidx.leanback.preference.LeanbackPreferenceFragmentCompat
+import androidx.preference.SeekBarPreference
+import java.util.Locale
 
 class SettingsUiFragment : LeanbackPreferenceFragmentCompat() {
     override fun onCreatePreferences(
@@ -9,5 +11,31 @@ class SettingsUiFragment : LeanbackPreferenceFragmentCompat() {
         rootKey: String?,
     ) {
         setPreferencesFromResource(R.xml.ui_preferences, rootKey)
+
+        val videoDelayPref =
+            findPreference<SeekBarPreference>(getString(R.string.pref_key_ui_card_overlay_delay))!!
+        setVideoDelaySummary(videoDelayPref, videoDelayPref.value)
+        videoDelayPref.setOnPreferenceChangeListener { _, newValue ->
+            setVideoDelaySummary(videoDelayPref, newValue)
+            true
+        }
+    }
+
+    private fun setVideoDelaySummary(
+        pref: SeekBarPreference,
+        value: Any,
+    ) {
+        val newValue = value.toString().toInt()
+        pref.summary =
+            if (newValue > 0) {
+                String.format(
+                    Locale.getDefault(),
+                    "%.1f %s",
+                    newValue / 1000.0,
+                    getString(R.string.stashapp_seconds),
+                )
+            } else {
+                "No delay"
+            }
     }
 }

app/src/main/java/com/github/damontecres/stashapp/presenters/TagPresenter.kt
@@ -30,7 +30,7 @@ class TagPresenter(callback: LongClickCallBack<TagData>? = null) :
         dataTypeMap[DataType.GALLERY] = item.gallery_count
 
         cardView.setUpExtraRow(dataTypeMap, null)
-        cardView.hideOverlayOnSelection = false
+//        cardView.hideOverlayOnSelection = false
 
         cardView.titleText = item.name
         cardView.contentText = item.description

app/src/main/java/com/github/damontecres/stashapp/views/models/ServerViewModel.kt
@@ -34,7 +34,19 @@ class ServerViewModel : ViewModel() {
         val showRatings = manager.getBoolean(context.getString(R.string.pref_key_show_rating), true)
         val imageCrop =
             manager.getBoolean(context.getString(R.string.pref_key_crop_card_images), true)
-        return Objects.hash(maxSearchResults, playVideoPreviews, columns, showRatings, imageCrop)
+        val videoDelay =
+            manager.getInt(
+                context.getString(R.string.pref_key_ui_card_overlay_delay),
+                context.resources.getInteger(R.integer.pref_key_ui_card_overlay_delay_default),
+            )
+        return Objects.hash(
+            maxSearchResults,
+            playVideoPreviews,
+            columns,
+            showRatings,
+            imageCrop,
+            videoDelay,
+        )
     }
 
     fun refresh() {

app/src/main/res/values/preferences.xml
@@ -26,4 +26,6 @@
     <string name="pref_key_ui_tag_tabs" translatable="false">ui.tabs.tag</string>
 
     <string name="pref_key_ui_remember_tab" translatable="false">ui.rememberTab</string>
+    <string name="pref_key_ui_card_overlay_delay" translatable="false">ui.card.delayVideoDuration</string>
+    <integer name="pref_key_ui_card_overlay_delay_default">1000</integer>
 </resources>

app/src/main/res/xml/ui_preferences.xml
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<androidx.preference.PreferenceScreen xmlns:app="http://schemas.android.com/apk/res-auto">
+<androidx.preference.PreferenceScreen xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:android="http://schemas.android.com/apk/res/android">
 
     <PreferenceCategory app:title="Advanced UI">
         <SwitchPreference
@@ -8,6 +9,13 @@
             app:summaryOn="Start on previously selected tab"
             app:summaryOff="Start on first tab"
             app:defaultValue="false" />
+        <SeekBarPreference
+            app:key="@string/pref_key_ui_card_overlay_delay"
+            app:title="Delay before playing card preview"
+            app:seekBarIncrement="100"
+            app:showSeekBarValue="false"
+            android:max="5000"
+            app:defaultValue="@integer/pref_key_ui_card_overlay_delay_default" />
     </PreferenceCategory>
 
     <PreferenceCategory

commit b614fffcc28b9acbad7fcb3306efaa2477ac0625

Fixes a crash when binding data to cards (#441)
Fixes a crash introduced in #440 when binding a card view but the card
hasn't been attached yet.

app/src/main/java/com/github/damontecres/stashapp/presenters/StashImageCardView.kt
@@ -81,6 +81,9 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
     private val animateTime = resources.getInteger(android.R.integer.config_mediumAnimTime).toLong()
 
     var videoUrl: String? = null
+        set(value) {
+            field = if (value.isNotNullOrBlank()) value else null
+        }
     var videoPosition = -1L
 
     private var videoView: PlayerView? = null
@@ -165,13 +168,19 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
         }
         if (selected) {
             if (videoDelay > 0) {
-                delayJob =
-                    findViewTreeLifecycleOwner()!!.lifecycleScope.launch(Dispatchers.IO + StashCoroutineExceptionHandler()) {
-                        delay(videoDelay)
-                        withContext(Dispatchers.Main) {
-                            hideOverlayAndPlayVideo()
+                val scope = findViewTreeLifecycleOwner()?.lifecycleScope
+                if (scope != null) {
+                    delayJob =
+                        scope.launch(Dispatchers.IO + StashCoroutineExceptionHandler()) {
+                            delay(videoDelay)
+                            withContext(Dispatchers.Main) {
+                                hideOverlayAndPlayVideo()
+                            }
                         }
-                    }
+                } else {
+                    // No lifecycle scope
+                    hideOverlayAndPlayVideo()
+                }
             } else {
                 // No delay, so do it immediately
                 hideOverlayAndPlayVideo()
@@ -309,7 +318,9 @@ class StashImageCardView(context: Context) : ImageCardView(context) {
     }
 
     private fun hideOverlayAndPlayVideo() {
-        videoView?.player?.play()
+        if (videoUrl != null) {
+            videoView?.player?.play()
+        }
         cardOverlay.clearAnimation()
         cardOverlay.animateToInvisible(durationMs = animateTime)
     }

app/src/main/java/com/github/damontecres/stashapp/presenters/StashPresenter.kt
@@ -58,9 +58,6 @@ abstract class StashPresenter<T>(private val callback: LongClickCallBack<T>? = n
 
             cardView.mainImageView.visibility = View.VISIBLE
             doOnBindViewHolder(viewHolder.view as StashImageCardView, item as T)
-            if (cardView.isSelected) {
-                cardView.isSelected = true
-            }
         }
     }
 