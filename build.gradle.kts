037c0bf1406a74c5d9076d0b1a2b730b9ef8803f
damontecres <154766448+damontecres@users.noreply.github.com>
2024-10-25 14:31:16 -0400

    Migrate to gradle version catalog (#443)
    No user facing changes
    
    Migrates to using gradle version catalog to share dependencies between
    modules more easily.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.ksp) apply false
 }
