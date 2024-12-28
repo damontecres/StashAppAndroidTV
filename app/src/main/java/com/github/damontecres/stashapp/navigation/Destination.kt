237fd8b3c5bab31c15ea2640a27a486b3f91c145
Damontecres <154766448+damontecres@users.noreply.github.com>
2024-12-28 10:19:43 -0500

    Fix crash on clicking containing/sub groups

--- app/src/main/java/com/github/damontecres/stashapp/navigation/Destination.kt
+++ app/src/main/java/com/github/damontecres/stashapp/navigation/Destination.kt
@@ -1,18 +1,24 @@
 package com.github.damontecres.stashapp.navigation
 
+import com.github.damontecres.stashapp.api.fragment.ExtraImageData
+import com.github.damontecres.stashapp.api.fragment.FullMarkerData
 import com.github.damontecres.stashapp.api.fragment.FullSceneData
 import com.github.damontecres.stashapp.api.fragment.GalleryData
 import com.github.damontecres.stashapp.api.fragment.GroupData
 import com.github.damontecres.stashapp.api.fragment.ImageData
+import com.github.damontecres.stashapp.api.fragment.JobData
 import com.github.damontecres.stashapp.api.fragment.MarkerData
 import com.github.damontecres.stashapp.api.fragment.PerformerData
+import com.github.damontecres.stashapp.api.fragment.SavedFilterData
 import com.github.damontecres.stashapp.api.fragment.SlimPerformerData
 import com.github.damontecres.stashapp.api.fragment.SlimSceneData
 import com.github.damontecres.stashapp.api.fragment.SlimTagData
 import com.github.damontecres.stashapp.api.fragment.StudioData
 import com.github.damontecres.stashapp.api.fragment.TagData
+import com.github.damontecres.stashapp.api.fragment.VideoFileData
 import com.github.damontecres.stashapp.api.fragment.VideoSceneData
 import com.github.damontecres.stashapp.data.DataType
+import com.github.damontecres.stashapp.data.GroupRelationshipData
 import com.github.damontecres.stashapp.data.StashData
 import com.github.damontecres.stashapp.playback.PlaybackMode
 import com.github.damontecres.stashapp.suppliers.FilterArgs
@@ -134,11 +140,14 @@ sealed class Destination {
                 is SlimSceneData, is FullSceneData, is VideoSceneData -> DataType.SCENE
                 is PerformerData, is SlimPerformerData -> DataType.PERFORMER
                 is TagData, is SlimTagData -> DataType.TAG
-                is GroupData -> DataType.GROUP
-                is ImageData -> DataType.IMAGE
+                is GroupData, is GroupRelationshipData -> DataType.GROUP
+                is ImageData, is ExtraImageData -> DataType.IMAGE
                 is GalleryData -> DataType.GALLERY
                 is StudioData -> DataType.STUDIO
-                is MarkerData -> DataType.MARKER
+                is MarkerData, is FullMarkerData -> DataType.MARKER
+
+                // TODO these probably shouldn't be StashData?
+                is JobData, is SavedFilterData, is VideoFileData -> throw IllegalArgumentException("Class not mapped: ${item::class}")
                 else -> throw IllegalArgumentException("Class not mapped: ${item::class}")
             }
     }
