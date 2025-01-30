47feec701f69cd458bcdc7b785ebebd9cfd4c26e
Damontecres <154766448+damontecres@users.noreply.github.com>
2025-01-30 11:11:12 -0500

    Add favorite/rating to performer

--- app/src/main/java/com/github/damontecres/stashapp/ui/components/PerformerPage.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ui/components/PerformerPage.kt
@@ -1,12 +1,15 @@
 package com.github.damontecres.stashapp.ui.components
 
 import android.os.Build
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,12 +34,16 @@ import com.github.damontecres.stashapp.data.DataType
 import com.github.damontecres.stashapp.suppliers.FilterArgs
 import com.github.damontecres.stashapp.ui.TabPage
 import com.github.damontecres.stashapp.ui.TabProvider
+import com.github.damontecres.stashapp.util.MutationEngine
 import com.github.damontecres.stashapp.util.PageFilterKey
 import com.github.damontecres.stashapp.util.QueryEngine
+import com.github.damontecres.stashapp.util.StashCoroutineExceptionHandler
 import com.github.damontecres.stashapp.util.StashServer
 import com.github.damontecres.stashapp.util.ageInYears
 import com.github.damontecres.stashapp.util.isNotNullOrBlank
+import com.github.damontecres.stashapp.util.showSetRatingToast
 import com.github.damontecres.stashapp.views.parseTimeToString
+import kotlinx.coroutines.launch
 import kotlin.math.floor
 import kotlin.math.roundToInt
 
@@ -49,10 +56,18 @@ fun PerformerPage(
     modifier: Modifier = Modifier,
 ) {
     var performer by remember { mutableStateOf<PerformerData?>(null) }
+    // Remember separately so we don't have refresh the whole page
+    var favorite by remember { mutableStateOf(false) }
+    var rating100 by remember { mutableIntStateOf(0) }
     LaunchedEffect(id) {
         performer = QueryEngine(server).getPerformer(id)
+        performer?.let {
+            favorite = it.favorite
+            rating100 = it.rating100 ?: 0
+        }
     }
     val context = LocalContext.current
+    val scope = rememberCoroutineScope()
 
     fun createTab(
         dataType: DataType,
@@ -69,18 +84,62 @@ fun PerformerPage(
             )
         }
 
-    performer?.let { performer ->
+    performer?.let { perf ->
         val performers =
             Optional.present(
                 MultiCriterionInput(
-                    value = Optional.present(listOf(performer.id)),
+                    value = Optional.present(listOf(perf.id)),
                     modifier = CriterionModifier.INCLUDES_ALL,
                 ),
             )
         val tabs =
             listOf(
                 TabProvider(stringResource(R.string.stashapp_details)) {
-                    PerformerDetails(performer, Modifier.fillMaxSize())
+                    PerformerDetails(
+                        modifier = Modifier.fillMaxSize(),
+                        perf = perf,
+                        favorite = favorite,
+                        favoriteClick = {
+                            val mutationEngine = MutationEngine(server)
+                            scope.launch(StashCoroutineExceptionHandler(autoToast = true)) {
+                                val newPerf =
+                                    mutationEngine.updatePerformer(
+                                        performerId = perf.id,
+                                        favorite = !favorite,
+                                    )
+                                if (newPerf != null) {
+                                    favorite = newPerf.favorite
+                                    if (newPerf.favorite) {
+                                        Toast
+                                            .makeText(
+                                                context,
+                                                "Performer favorited!",
+                                                Toast.LENGTH_SHORT,
+                                            ).show()
+                                    }
+                                }
+                            }
+                        },
+                        rating100 = rating100,
+                        rating100Click = { newRating100 ->
+                            val mutationEngine = MutationEngine(server)
+                            scope.launch(StashCoroutineExceptionHandler(autoToast = true)) {
+                                val newPerf =
+                                    mutationEngine.updatePerformer(
+                                        performerId = perf.id,
+                                        rating100 = newRating100,
+                                    )
+                                if (newPerf != null) {
+                                    rating100 = newPerf.rating100 ?: 0
+                                    showSetRatingToast(
+                                        context,
+                                        newPerf.rating100 ?: 0,
+                                        server.serverPreferences.ratingsAsStars,
+                                    )
+                                }
+                            }
+                        },
+                    )
                 },
                 createTab(
                     DataType.SCENE,
@@ -140,12 +199,12 @@ fun PerformerPage(
                 .Builder()
                 .apply {
                     withStyle(SpanStyle(color = Color.White, fontSize = 40.sp)) {
-                        append(performer.name)
+                        append(perf.name)
                     }
-                    if (performer.disambiguation.isNotNullOrBlank()) {
+                    if (perf.disambiguation.isNotNullOrBlank()) {
                         withStyle(SpanStyle(color = Color.LightGray, fontSize = 24.sp)) {
                             append(" ")
-                            append(performer.disambiguation)
+                            append(perf.disambiguation)
                         }
                     }
                 }.toAnnotatedString()
@@ -156,6 +215,10 @@ fun PerformerPage(
 @Composable
 fun PerformerDetails(
     perf: PerformerData,
+    favorite: Boolean,
+    favoriteClick: () -> Unit,
+    rating100: Int,
+    rating100Click: (rating100: Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val rows =
@@ -216,9 +279,9 @@ fun PerformerDetails(
         modifier = modifier,
         imageUrl = perf.image_path,
         tableRows = rows,
-        favorite = perf.favorite,
-        favoriteClick = {},
-        rating100 = perf.rating100 ?: 0,
-        rating100Click = {},
+        favorite = favorite,
+        favoriteClick = favoriteClick,
+        rating100 = rating100,
+        rating100Click = rating100Click,
     )
 }
