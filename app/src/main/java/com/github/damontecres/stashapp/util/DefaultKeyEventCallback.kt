8211f5e323d746dd04dbe4fbcdfda16844029616
damontecres <154766448+damontecres@users.noreply.github.com>
2025-03-22 11:44:56 -0400

    Ensure fragments are passing key events down the stack (#581)
    This fixes some button presses (play, long-press, etc) not working on
    some pages.

--- app/src/main/java/com/github/damontecres/stashapp/util/DefaultKeyEventCallback.kt
+++ app/src/main/java/com/github/damontecres/stashapp/util/DefaultKeyEventCallback.kt
@@ -24,3 +24,28 @@ interface DefaultKeyEventCallback : KeyEvent.Callback {
         event: KeyEvent,
     ): Boolean = false
 }
+
+interface DelegateKeyEventCallback : KeyEvent.Callback {
+    val keyEventDelegate: KeyEvent.Callback?
+
+    override fun onKeyDown(
+        keyCode: Int,
+        event: KeyEvent,
+    ): Boolean = keyEventDelegate?.onKeyDown(keyCode, event) ?: false
+
+    override fun onKeyUp(
+        keyCode: Int,
+        event: KeyEvent,
+    ): Boolean = keyEventDelegate?.onKeyUp(keyCode, event) ?: false
+
+    override fun onKeyLongPress(
+        keyCode: Int,
+        event: KeyEvent,
+    ): Boolean = keyEventDelegate?.onKeyLongPress(keyCode, event) ?: false
+
+    override fun onKeyMultiple(
+        keyCode: Int,
+        count: Int,
+        event: KeyEvent,
+    ): Boolean = keyEventDelegate?.onKeyMultiple(keyCode, count, event) ?: false
+}
