8b06d3c9330790a723ccfe30bcbcd0ea29dbf0f1
Damontecres <154766448+damontecres@users.noreply.github.com>
2025-04-11 21:12:03 -0400

    Split up ImagePage functions

---
+++ app/src/main/java/com/github/damontecres/stashapp/ui/components/image/ImageDetailsHeader.kt
@@ -0,0 +1,211 @@
+package com.github.damontecres.stashapp.ui.components.image
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.media3.common.Player
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
+import com.github.damontecres.stashapp.R
+import com.github.damontecres.stashapp.api.fragment.ImageData
+import com.github.damontecres.stashapp.ui.ComposeUiConfig
+import com.github.damontecres.stashapp.ui.components.DotSeparatedRow
+import com.github.damontecres.stashapp.ui.components.ItemOnClicker
+import com.github.damontecres.stashapp.ui.components.StarRating
+import com.github.damontecres.stashapp.ui.components.TitleValueText
+import com.github.damontecres.stashapp.util.isImageClip
+import com.github.damontecres.stashapp.util.isNotNullOrBlank
+import com.github.damontecres.stashapp.util.listOfNotNullOrBlank
+import com.github.damontecres.stashapp.util.resolutionName
+import com.github.damontecres.stashapp.util.titleOrFilename
+import com.github.damontecres.stashapp.views.durationToString
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ImageDetailsHeader(
+    player: Player,
+    image: ImageData,
+    rating100: Int,
+    oCount: Int,
+    uiConfig: ComposeUiConfig,
+    itemOnClick: ItemOnClicker<Any>,
+    moreOnClick: () -> Unit,
+    oCounterOnClick: () -> Unit,
+    oCounterOnLongClick: () -> Unit,
+    onRatingChange: (Int) -> Unit,
+    onZoom: (Float) -> Unit,
+    onRotate: (Int) -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val bringIntoViewRequester = remember { BringIntoViewRequester() }
+    val scope = rememberCoroutineScope()
+
+    Column(
+        modifier =
+            modifier
+                .fillMaxWidth(0.8f)
+                .height(440.dp)
+                .bringIntoViewRequester(bringIntoViewRequester),
+    ) {
+        // Title
+        Text(
+            text = image.titleOrFilename ?: "",
+//                        color = MaterialTheme.colorScheme.onBackground,
+            color = Color.LightGray,
+            style =
+                MaterialTheme.typography.displayMedium.copy(
+                    shadow =
+                        Shadow(
+                            color = Color.DarkGray,
+                            offset = Offset(5f, 2f),
+                            blurRadius = 2f,
+                        ),
+                ),
+        )
+        Column(
+            modifier = Modifier.alpha(0.75f),
+        ) {
+            // Rating
+            StarRating(
+                rating100 = rating100,
+                precision = uiConfig.starPrecision,
+                onRatingChange = onRatingChange,
+                enabled = true,
+                modifier =
+                    Modifier
+                        .height(30.dp),
+            )
+            // Quick info
+            val imageFile = image.visual_files.firstOrNull()?.onImageFile
+            val videoFile = image.visual_files.firstOrNull()?.onVideoFile
+
+            val imageRes = imageFile?.let { "${it.width}x${it.height}" }
+
+            DotSeparatedRow(
+                modifier = Modifier.padding(top = 6.dp),
+                textStyle = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                texts =
+                    listOfNotNullOrBlank(
+                        image.date,
+                        imageRes,
+                        videoFile?.let { durationToString(it.duration) },
+                        videoFile?.resolutionName(),
+                    ),
+            )
+            // Description
+            if (image.details.isNotNullOrBlank()) {
+                val interactionSource = remember { MutableInteractionSource() }
+                val isFocused = interactionSource.collectIsFocusedAsState().value
+                val bgColor =
+                    if (isFocused) {
+                        MaterialTheme.colorScheme.secondaryContainer.copy(alpha = .75f)
+                    } else {
+                        Color.Unspecified
+                    }
+                var textOverflow by remember { mutableStateOf(false) }
+                var showDetailsDialog by remember { mutableStateOf(false) }
+                Box(
+                    modifier =
+                        Modifier
+                            .background(bgColor, shape = RoundedCornerShape(8.dp))
+                            .focusable(
+                                enabled = textOverflow,
+                                interactionSource = interactionSource,
+                            ).onFocusChanged {
+                                if (it.isFocused) {
+                                    scope.launch { bringIntoViewRequester.bringIntoView() }
+                                }
+                            }.clickable(enabled = textOverflow) { showDetailsDialog = true },
+                ) {
+                    Text(
+                        text = image.details,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        maxLines = 3,
+                        overflow = TextOverflow.Ellipsis,
+                        onTextLayout = { textLayoutResult ->
+                            textOverflow = textLayoutResult.hasVisualOverflow
+                        },
+                        modifier = Modifier.padding(8.dp),
+                    )
+                }
+            }
+            // Key-Values
+            Row(
+                modifier =
+                    Modifier
+                        .padding(top = 16.dp)
+                        .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                if (image.studio != null) {
+                    TitleValueText(
+                        stringResource(R.string.stashapp_studio),
+                        image.studio.name,
+                    )
+                }
+                if (image.code.isNotNullOrBlank()) {
+                    TitleValueText(
+                        stringResource(R.string.stashapp_scene_code),
+                        image.code,
+                    )
+                }
+                if (image.photographer.isNotNullOrBlank()) {
+                    TitleValueText(
+                        stringResource(R.string.stashapp_director),
+                        image.photographer,
+                    )
+                }
+            }
+        }
+        ImageControlsOverlay(
+            player = player,
+            isImageClip = image.isImageClip,
+            oCount = oCount,
+            bringIntoViewRequester = bringIntoViewRequester,
+            onZoom = onZoom,
+            onRotate = onRotate,
+            onReset = onReset,
+            moreOnClick = moreOnClick,
+            oCounterOnClick = oCounterOnClick,
+            oCounterOnLongClick = oCounterOnLongClick,
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+        )
+    }
+}
---
+++ app/src/main/java/com/github/damontecres/stashapp/ui/components/image/ImageOverlay.kt
@@ -0,0 +1,294 @@
+package com.github.damontecres.stashapp.ui.components.image
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.tv.material3.Icon
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
+import androidx.tv.material3.surfaceColorAtElevation
+import com.github.damontecres.stashapp.R
+import com.github.damontecres.stashapp.api.fragment.ImageData
+import com.github.damontecres.stashapp.api.fragment.PerformerData
+import com.github.damontecres.stashapp.api.fragment.StashData
+import com.github.damontecres.stashapp.api.fragment.TagData
+import com.github.damontecres.stashapp.data.DataType
+import com.github.damontecres.stashapp.data.OCounter
+import com.github.damontecres.stashapp.filter.extractTitle
+import com.github.damontecres.stashapp.ui.ComposeUiConfig
+import com.github.damontecres.stashapp.ui.Material3MainTheme
+import com.github.damontecres.stashapp.ui.components.DialogItem
+import com.github.damontecres.stashapp.ui.components.DialogPopup
+import com.github.damontecres.stashapp.ui.components.ItemOnClicker
+import com.github.damontecres.stashapp.ui.components.ItemsRow
+import com.github.damontecres.stashapp.ui.components.LongClicker
+import com.github.damontecres.stashapp.ui.pages.DialogParams
+import com.github.damontecres.stashapp.ui.pages.SearchForPage
+import com.github.damontecres.stashapp.ui.pages.SearchForParams
+import com.github.damontecres.stashapp.util.MutationEngine
+import com.github.damontecres.stashapp.util.StashServer
+import com.github.damontecres.stashapp.util.readOnlyModeDisabled
+
+@androidx.annotation.OptIn(UnstableApi::class)
+@Composable
+fun ImageOverlay(
+    server: StashServer,
+    player: Player,
+    slideshowControls: SlideshowControls,
+    slideshowEnabled: Boolean,
+    image: ImageData,
+    tags: List<TagData>,
+    performers: List<PerformerData>,
+    rating100: Int,
+    oCount: Int,
+    uiConfig: ComposeUiConfig,
+    itemOnClick: ItemOnClicker<Any>,
+    oCountAction: (action: suspend MutationEngine.(String) -> OCounter) -> Unit,
+    onRatingChange: (Int) -> Unit,
+    longClicker: LongClicker<Any>,
+    addItem: (item: StashData) -> Unit,
+    removeItem: (item: StashData) -> Unit,
+    onZoom: (Float) -> Unit,
+    onRotate: (Int) -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    var showDialog by remember { mutableStateOf<DialogParams?>(null) }
+    var searchForDataType by remember { mutableStateOf<SearchForParams?>(null) }
+
+    val removeLongClicker =
+        LongClicker<Any> { item, filterAndPosition ->
+            item as StashData
+            showDialog =
+                DialogParams(
+                    title = extractTitle(item) ?: "",
+                    fromLongClick = true,
+                    items =
+                        buildList {
+                            add(
+                                DialogItem("Go to", Icons.Default.PlayArrow) {
+                                    itemOnClick.onClick(
+                                        item,
+                                        filterAndPosition,
+                                    )
+                                },
+                            )
+                            if (readOnlyModeDisabled()) {
+                                add(
+                                    DialogItem(
+                                        onClick = { removeItem(item) },
+                                        headlineContent = {
+                                            Text(stringResource(R.string.stashapp_actions_remove))
+                                        },
+                                        leadingContent = {
+                                            Icon(
+                                                imageVector = Icons.Default.Delete,
+                                                contentDescription = stringResource(R.string.stashapp_actions_remove),
+                                                tint = Color.Red,
+                                            )
+                                        },
+                                    ),
+                                )
+                            }
+                        },
+                )
+        }
+
+    val startStopSlideshow =
+        DialogItem(
+            headlineContent = {
+                Text(
+                    text =
+                        if (slideshowEnabled) {
+                            stringResource(R.string.stop_slideshow)
+                        } else {
+                            stringResource(R.string.play_slideshow)
+                        },
+                )
+            },
+            leadingContent = {
+                Icon(
+                    painter = painterResource(if (slideshowEnabled) R.drawable.baseline_pause_24 else R.drawable.baseline_play_arrow_24),
+                    contentDescription = null,
+                )
+            },
+            onClick = { if (slideshowEnabled) slideshowControls.stopSlideshow() else slideshowControls.startSlideshow() },
+        )
+
+    LazyColumn(
+        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 135.dp),
+        modifier = modifier.fillMaxSize(),
+    ) {
+        item {
+            ImageDetailsHeader(
+                player = player,
+                image = image,
+                rating100 = rating100,
+                oCount = oCount,
+                uiConfig = uiConfig,
+                itemOnClick = itemOnClick,
+                moreOnClick = {
+                    showDialog =
+                        DialogParams(
+                            title = context.getString(R.string.more) + "...",
+                            fromLongClick = false,
+                            items =
+                                buildList {
+                                    add(startStopSlideshow)
+                                    if (readOnlyModeDisabled()) {
+                                        add(
+                                            DialogItem(
+                                                context.getString(R.string.add_performer),
+                                                DataType.PERFORMER.iconStringId,
+                                            ) {
+                                                searchForDataType =
+                                                    SearchForParams(DataType.PERFORMER)
+                                            },
+                                        )
+                                        add(
+                                            DialogItem(
+                                                context.getString(R.string.add_tag),
+                                                DataType.TAG.iconStringId,
+                                            ) {
+                                                searchForDataType = SearchForParams(DataType.TAG)
+                                            },
+                                        )
+                                    }
+                                },
+                        )
+                },
+                oCounterOnClick = { oCountAction.invoke(MutationEngine::incrementImageOCounter) },
+                oCounterOnLongClick = {
+                    showDialog =
+                        DialogParams(
+                            title = context.getString(R.string.stashapp_o_counter),
+                            fromLongClick = true,
+                            items =
+                                listOf(
+                                    DialogItem(context.getString(R.string.increment)) {
+                                        oCountAction.invoke(MutationEngine::incrementImageOCounter)
+                                    },
+                                    DialogItem(context.getString(R.string.decrement)) {
+                                        oCountAction.invoke(MutationEngine::decrementImageOCounter)
+                                    },
+                                    DialogItem(context.getString(R.string.reset)) {
+                                        oCountAction.invoke(MutationEngine::resetImageOCounter)
+                                    },
+                                ),
+                        )
+                },
+                onRatingChange = onRatingChange,
+                onZoom = onZoom,
+                onRotate = onRotate,
+                onReset = onReset,
+                modifier = Modifier,
+            )
+        }
+        val startPadding = 24.dp
+        val bottomPadding = 16.dp
+
+        if (performers.isNotEmpty()) {
+            item {
+                ItemsRow(
+                    title = stringResource(R.string.stashapp_performers),
+                    items = performers,
+                    uiConfig = uiConfig,
+                    itemOnClick = itemOnClick,
+                    longClicker = removeLongClicker,
+                    modifier = Modifier.padding(start = startPadding, bottom = bottomPadding),
+                )
+            }
+        }
+        if (tags.isNotEmpty()) {
+            item {
+                ItemsRow(
+                    title = stringResource(R.string.stashapp_tags),
+                    items = tags,
+                    uiConfig = uiConfig,
+                    itemOnClick = itemOnClick,
+                    longClicker = removeLongClicker,
+                    modifier = Modifier.padding(start = startPadding, bottom = bottomPadding),
+                )
+            }
+        }
+        item {
+            ImageDetailsFooter(
+                image,
+                Modifier.padding(start = startPadding, bottom = bottomPadding, top = 24.dp),
+            )
+        }
+    }
+    showDialog?.let { params ->
+        DialogPopup(
+            showDialog = true,
+            title = params.title,
+            dialogItems = params.items,
+            onDismissRequest = { showDialog = null },
+            waitToLoad = params.fromLongClick,
+        )
+    }
+    searchForDataType?.let { params ->
+        Material3MainTheme {
+            Dialog(
+                onDismissRequest = { searchForDataType = null },
+                properties =
+                    DialogProperties(
+                        usePlatformDefaultWidth = false,
+                    ),
+            ) {
+                val elevatedContainerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                val color = MaterialTheme.colorScheme.secondaryContainer
+                Box(
+                    Modifier
+                        .fillMaxSize(.9f)
+                        .graphicsLayer {
+                            this.clip = true
+                            this.shape = RoundedCornerShape(28.0.dp)
+                        }.drawBehind { drawRect(color = color) }
+                        .background(MaterialTheme.colorScheme.secondaryContainer)
+                        .padding(PaddingValues(12.dp)),
+                    propagateMinConstraints = true,
+                ) {
+                    SearchForPage(
+                        server = server,
+                        title = "Add " + stringResource(params.dataType.stringId),
+                        searchId = params.id,
+                        dataType = params.dataType,
+                        itemOnClick = { id, item ->
+                            // Close dialog
+                            searchForDataType = null
+                            addItem.invoke(item)
+                        },
+                        uiConfig = uiConfig,
+                        modifier = Modifier.fillMaxSize(),
+                    )
+                }
+            }
+        }
+    }
+}
