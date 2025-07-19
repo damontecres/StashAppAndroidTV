2cc66feddac432d5df5ee05308cf83a1fdf2af7f
Damontecres <154766448+damontecres@users.noreply.github.com>
2025-07-19 17:24:36 -0400

    Refactor add server dialog

--- app/src/main/java/com/github/damontecres/stashapp/ui/components/server/AddServerContent.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ui/components/server/AddServerContent.kt
@@ -13,12 +13,14 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -26,6 +28,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.tv.material3.Button
 import androidx.tv.material3.MaterialTheme
 import androidx.tv.material3.Text
@@ -41,62 +44,28 @@ import com.github.damontecres.stashapp.util.TestResult
 import com.github.damontecres.stashapp.util.getPreference
 import com.github.damontecres.stashapp.util.isNotNullOrBlank
 import com.github.damontecres.stashapp.util.testStashConnection
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+private const val TAG = "AddServerContent"
 
 @Composable
 fun AddServer(
     currentServerUrls: List<String>,
     onSubmit: (StashServer) -> Unit,
     modifier: Modifier = Modifier,
+    viewModel: ManageServersViewModel = viewModel(),
 ) {
     val context = LocalContext.current
-    val scope = rememberCoroutineScope()
 
     var serverUrl by remember { mutableStateOf("") }
     var apiKey by remember { mutableStateOf<String?>(null) }
     var showApiKey by remember { mutableStateOf(false) }
-    var result by remember { mutableStateOf<TestResult?>(null) }
-    var errorMessage by remember { mutableStateOf<String?>(null) }
-
-    var checkingConnection by remember { mutableStateOf(false) }
-
     val trustCerts = remember { getPreference(context, R.string.pref_key_trust_certs, false) }
 
+    val connectionState by viewModel.connectionState.observeAsState(ConnectionState.Inactive)
+
     LaunchedEffect(serverUrl, apiKey) {
-        result = null
-        errorMessage = null
-        if (serverUrl.isNotNullOrBlank()) {
-            try {
-                checkingConnection = true
-                errorMessage = null
-                delay(500L)
-                if (serverUrl in currentServerUrls) {
-                    errorMessage = "Duplicate server"
-                } else {
-                    result =
-                        if (serverUrl.isNotNullOrBlank()) {
-                            val apolloClient =
-                                StashClient.createTestApolloClient(
-                                    context,
-                                    StashServer(serverUrl, apiKey?.ifBlank { null }),
-                                    trustCerts,
-                                )
-                            testStashConnection(context, false, apolloClient)
-                        } else {
-                            null
-                        }
-                    errorMessage = result?.message
-                }
-            } catch (ex: Exception) {
-                result = TestResult.Error(ex.localizedMessage, ex)
-                errorMessage = ex.localizedMessage
-            } finally {
-                checkingConnection = false
-            }
-        } else {
-            checkingConnection = false
-        }
+        viewModel.testServer(serverUrl, apiKey, trustCerts)
     }
 
     val labelWidth = 64.dp
@@ -108,20 +77,22 @@ fun AddServer(
     ) {
         stickyHeader {
             Text(
-                text = "Add server",
+                text = stringResource(R.string.add_server),
                 color = MaterialTheme.colorScheme.onSecondaryContainer,
                 style = MaterialTheme.typography.displaySmall,
                 textAlign = TextAlign.Center,
                 modifier = Modifier.fillParentMaxWidth(),
             )
         }
+
+        // Server url
         item {
             Row(
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 Text(
-                    text = "URL:",
+                    text = stringResource(R.string.stashapp_url),
                     color = MaterialTheme.colorScheme.onSecondaryContainer,
                     style = MaterialTheme.typography.titleMedium,
                     modifier = Modifier.width(labelWidth),
@@ -138,14 +109,21 @@ fun AddServer(
                     keyboardActions = KeyboardActions(),
                     leadingIcon = null,
                     isInputValid = {
-                        result == null ||
-                            result is TestResult.Success ||
-                            result is TestResult.AuthRequired
+                        connectionState.let {
+                            when (it) {
+                                ConnectionState.DuplicateServer -> false
+                                ConnectionState.Inactive -> true
+                                is ConnectionState.Result -> it.canConnect
+                                ConnectionState.Testing -> true
+                            }
+                        }
                     },
                     modifier = Modifier.fillMaxWidth(),
                 )
             }
         }
+
+        // API Key
         item {
             Row(
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -153,7 +131,7 @@ fun AddServer(
                 modifier = Modifier.animateItem(),
             ) {
                 Text(
-                    text = "API Key:",
+                    text = stringResource(R.string.stashapp_config_general_auth_api_key),
                     color = MaterialTheme.colorScheme.onSecondaryContainer,
                     style = MaterialTheme.typography.titleMedium,
                     modifier = Modifier.width(labelWidth),
@@ -170,14 +148,24 @@ fun AddServer(
                     keyboardActions = KeyboardActions(),
                     leadingIcon = null,
                     enabled =
-                        result is TestResult.AuthRequired || apiKey.isNotNullOrBlank(),
+                        connectionState.let { it is ConnectionState.Result && it.testResult is TestResult.AuthRequired } ||
+                            apiKey.isNotNullOrBlank(),
                     isInputValid = {
-                        apiKey.isNullOrEmpty() || result is TestResult.Success
+                        apiKey.isNullOrEmpty() ||
+                            connectionState.let {
+                                when (it) {
+                                    ConnectionState.DuplicateServer -> true
+                                    ConnectionState.Inactive -> true
+                                    is ConnectionState.Result -> it.testResult is TestResult.Success
+                                    ConnectionState.Testing -> true
+                                }
+                            }
                     },
                     modifier = Modifier.fillMaxWidth(),
                 )
             }
         }
+
         item {
             Box(
                 contentAlignment = Alignment.CenterEnd,
@@ -195,63 +183,108 @@ fun AddServer(
             }
         }
 
-        errorMessage?.let {
-            if (it.isNotNullOrBlank()) {
-                item {
-                    Text(
-                        text = it,
-                        color = MaterialTheme.colorScheme.error,
-                    )
-                }
-            }
+        item {
+            StatusText(connectionState)
         }
+
         item {
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier.fillParentMaxWidth(),
             ) {
-                Button(
-                    onClick = {
-                        scope.launch(StashCoroutineExceptionHandler()) {
-                            try {
-                                checkingConnection = true
-                                val server = StashServer(serverUrl, apiKey?.ifBlank { null })
-                                if (serverUrl.isNotNullOrBlank()) {
-                                    val apolloClient =
-                                        StashClient.createTestApolloClient(
-                                            context,
-                                            server,
-                                            trustCerts,
-                                        )
-                                    val testResult =
-                                        testStashConnection(
-                                            context,
-                                            false,
-                                            apolloClient,
-                                        )
-                                    if (testResult is TestResult.Success) {
-                                        onSubmit.invoke(server)
-                                    }
-                                }
-                            } finally {
-                                checkingConnection = false
-                            }
-                        }
-                    },
-                    enabled = !checkingConnection && result is TestResult.Success && serverUrl !in currentServerUrls,
+                SubmitServerButton(
+                    connectionState = connectionState,
+                    serverUrl = serverUrl,
+                    apiKey = apiKey,
+                    trustCerts = trustCerts,
+                    onSubmit = onSubmit,
                     modifier = Modifier,
-                ) {
-                    if (checkingConnection) {
-                        CircularProgress(Modifier.size(32.dp), false)
-                    } else {
-                        Text(
-                            text = stringResource(R.string.stashapp_actions_submit),
-                            color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        )
-                    }
-                }
+                )
             }
         }
+    }
+}
+
+@Composable
+fun SubmitServerButton(
+    connectionState: ConnectionState,
+    serverUrl: String?,
+    apiKey: String?,
+    trustCerts: Boolean,
+    onSubmit: (StashServer) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    var testing by remember { mutableStateOf(false) }
+
+    Button(
+        onClick = {
+            scope.launch(StashCoroutineExceptionHandler()) {
+                try {
+                    testing = true
+                    if (serverUrl.isNotNullOrBlank()) {
+                        val server = StashServer(serverUrl, apiKey?.ifBlank { null })
+                        val apolloClient =
+                            StashClient.createTestApolloClient(
+                                context,
+                                server,
+                                trustCerts,
+                            )
+                        val testResult =
+                            testStashConnection(
+                                context,
+                                false,
+                                apolloClient,
+                            )
+                        if (testResult is TestResult.Success) {
+                            onSubmit.invoke(server)
+                        }
+                    }
+                } finally {
+                    testing = false
+                }
+            }
+        },
+        enabled = !testing && connectionState.let { it is ConnectionState.Result && it.testResult is TestResult.Success },
+        modifier = modifier,
+    ) {
+        if (testing || connectionState is ConnectionState.Testing) {
+            CircularProgress(Modifier.size(32.dp), false)
+        } else {
+            Text(
+                text = stringResource(R.string.stashapp_actions_submit),
+//                color = MaterialTheme.colorScheme.onSecondaryContainer,
+            )
+        }
+    }
+}
+
+@Composable
+fun StatusText(
+    state: ConnectionState,
+    modifier: Modifier = Modifier,
+) {
+    val message =
+        state.let {
+            when (it) {
+                is ConnectionState.Result -> it.testResult.message.ifBlank { null }
+                is ConnectionState.DuplicateServer -> "Duplicate server"
+                else -> null
+            }
+        }
+    if (message.isNotNullOrBlank()) {
+        Text(
+            text = message,
+            color = MaterialTheme.colorScheme.error,
+            modifier = modifier,
+        )
+    } else if (state is ConnectionState.Result && state.testResult is TestResult.Success) {
+        Text(
+            text = stringResource(R.string.success),
+            color = Color.Green.copy(alpha = .66f),
+            modifier = modifier,
+        )
     }
 }
 
--- app/src/main/java/com/github/damontecres/stashapp/ui/components/server/ManageServersViewModel.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ui/components/server/ManageServersViewModel.kt
@@ -1,18 +1,26 @@
 package com.github.damontecres.stashapp.ui.components.server
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.damontecres.stashapp.StashApplication
+import com.github.damontecres.stashapp.util.StashClient
 import com.github.damontecres.stashapp.util.StashCoroutineExceptionHandler
 import com.github.damontecres.stashapp.util.StashServer
 import com.github.damontecres.stashapp.util.TestResult
+import com.github.damontecres.stashapp.util.isNotNullOrBlank
 import com.github.damontecres.stashapp.util.testStashConnection
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class ManageServersViewModel : ViewModel() {
     val allServers = MutableLiveData<List<StashServer>>(listOf())
     val serverStatus = MutableLiveData<Map<StashServer, ServerTestResult>>(mapOf())
+
+    val connectionState = MutableLiveData<ConnectionState>(ConnectionState.Inactive)
 
     init {
         val servers = StashServer.getAll(StashApplication.getApplication())
@@ -58,6 +66,49 @@ class ManageServersViewModel : ViewModel() {
         StashServer.addServer(StashApplication.getApplication(), server)
         val servers = StashServer.getAll(StashApplication.getApplication())
         allServers.value = servers
+    }
+
+    private var testServerJob: Job? = null
+
+    fun testServer(
+        serverUrl: String,
+        apiKey: String?,
+        trustCerts: Boolean,
+    ) {
+        testServerJob?.cancel()
+        testServerJob =
+            viewModelScope.launch(StashCoroutineExceptionHandler()) {
+                val context = StashApplication.getApplication()
+                connectionState.value = ConnectionState.Inactive
+                if (serverUrl.isNotNullOrBlank()) {
+                    if (serverUrl in allServers.value!!.map { it.url }) {
+                        connectionState.value = ConnectionState.DuplicateServer
+                    } else {
+                        connectionState.value = ConnectionState.Testing
+                        delay(500L)
+                        try {
+                            val apolloClient =
+                                StashClient.createTestApolloClient(
+                                    context,
+                                    StashServer(serverUrl, apiKey?.ifBlank { null }),
+                                    trustCerts,
+                                )
+                            val result = testStashConnection(context, false, apolloClient)
+                            connectionState.value = ConnectionState.Result(result)
+                        } catch (_: CancellationException) {
+                            connectionState.value = ConnectionState.Inactive
+                        } catch (ex: Exception) {
+                            connectionState.value =
+                                ConnectionState.Result(TestResult.Error(ex.localizedMessage, ex))
+                        }
+                    }
+                }
+                Log.d(TAG, "connectionState=$connectionState")
+            }
+    }
+
+    companion object {
+        private const val TAG = "ManageServersViewModel"
     }
 }
 
