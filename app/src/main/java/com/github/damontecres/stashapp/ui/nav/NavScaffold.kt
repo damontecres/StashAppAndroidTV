9415d0e7bdb1cdb89530483394ed07fa7275bc6e
Damontecres <154766448+damontecres@users.noreply.github.com>
2025-07-29 14:17:19 -0400

    Adjust title font sizes

--- app/src/main/java/com/github/damontecres/stashapp/ui/nav/NavScaffold.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ui/nav/NavScaffold.kt
@@ -1,5 +1,6 @@
 package com.github.damontecres.stashapp.ui.nav
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +18,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,15 +33,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.tv.material3.MaterialTheme
 import com.github.damontecres.stashapp.R
+import com.github.damontecres.stashapp.data.DataType
 import com.github.damontecres.stashapp.navigation.Destination
 import com.github.damontecres.stashapp.navigation.NavigationManagerCompose
 import com.github.damontecres.stashapp.ui.ComposeUiConfig
 import com.github.damontecres.stashapp.ui.FontAwesome
 import com.github.damontecres.stashapp.ui.components.ItemOnClicker
 import com.github.damontecres.stashapp.ui.components.LongClicker
+import com.github.damontecres.stashapp.ui.util.ScreenSize
+import com.github.damontecres.stashapp.ui.util.screenSize
 import com.github.damontecres.stashapp.util.StashServer
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 fun NavScaffold(
     server: StashServer,
@@ -56,25 +61,60 @@ fun NavScaffold(
     var expanded by remember { mutableStateOf(false) }
     var title by remember { mutableStateOf<AnnotatedString?>(null) }
 
+    Log.v("WindowSize", "screenSize=${screenSize()}")
+
+    val titleStyle =
+        when (screenSize()) {
+            ScreenSize.COMPACT -> androidx.compose.material3.MaterialTheme.typography.headlineSmall
+            ScreenSize.MEDIUM -> androidx.compose.material3.MaterialTheme.typography.headlineMedium
+            ScreenSize.EXPANDED -> androidx.compose.material3.MaterialTheme.typography.headlineLarge
+        }
+
     Scaffold(
         modifier = modifier,
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    when (selectedScreen) {
-                        is DrawerPage.HomePage,
-                        is DrawerPage.SearchPage,
-                        ->
-                            Text(
-                                text = stringResource(selectedScreen.name),
-                            )
+                    val title =
+                        when (selectedScreen) {
+                            is DrawerPage.HomePage,
+                            is DrawerPage.SearchPage,
+                            -> AnnotatedString(stringResource(selectedScreen.name))
 
-                        else ->
-                            title?.let {
-                                Text(
-                                    text = it,
-                                )
+                            else -> title
+                        }
+                    title?.let {
+                        val updatedTitle =
+                            if (destination is Destination.Item && destination.dataType == DataType.PERFORMER) {
+                                // TODO hack to adjust font sizes
+                                val newStyles =
+                                    if (title.spanStyles.size == 2) {
+                                        listOf(
+                                            title.spanStyles[0].let {
+                                                it.copy(item = it.item.copy(fontSize = titleStyle.fontSize))
+                                            },
+                                            title.spanStyles[1].let {
+                                                it.copy(item = it.item.copy(fontSize = titleStyle.fontSize * .75f))
+                                            },
+                                        )
+                                    } else if (title.spanStyles.size == 1) {
+                                        listOf(
+                                            title.spanStyles[0].let {
+                                                it.copy(item = it.item.copy(fontSize = titleStyle.fontSize))
+                                            },
+                                        )
+                                    } else {
+                                        listOf()
+                                    }
+
+                                AnnotatedString(title.text, newStyles, title.paragraphStyles)
+                            } else {
+                                it
                             }
+                        Text(
+                            text = updatedTitle,
+                            style = titleStyle,
+                        )
                     }
                 },
                 navigationIcon = {
