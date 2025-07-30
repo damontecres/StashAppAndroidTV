b564440334802c264cb3720d7b84876540b30118
damontecres <154766448+damontecres@users.noreply.github.com>
2025-07-30 08:49:57 -0400

    Support touch dragging the scrubber bar to seek in videos (#700)
    Swaps the d-pad controlled scrubber bar for a touch enabled one for
    non-TV devices
    
    The size and layout isn't perfect, but the functionality is good.

--- app/src/main/java/com/github/damontecres/stashapp/ui/components/playback/PlaybackOverlay.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ui/components/playback/PlaybackOverlay.kt
@@ -4,6 +4,7 @@ import androidx.annotation.IntRange
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
@@ -71,6 +72,7 @@ import com.github.damontecres.stashapp.ui.AppColors
 import com.github.damontecres.stashapp.ui.AppTheme
 import com.github.damontecres.stashapp.ui.ComposeUiConfig
 import com.github.damontecres.stashapp.ui.cards.RootCard
+import com.github.damontecres.stashapp.ui.compat.isNotTvDevice
 import com.github.damontecres.stashapp.ui.components.StarRatingPrecision
 import com.github.damontecres.stashapp.ui.indexOfFirstOrNull
 import com.github.damontecres.stashapp.ui.tryRequestFocus
@@ -163,6 +165,7 @@ fun PlaybackOverlay(
     val scope = rememberCoroutineScope()
     var seekProgress by remember { mutableFloatStateOf(-1f) }
     val seekBarFocused by seekBarInteractionSource.collectIsFocusedAsState()
+    var seekBarDragging by remember { mutableStateOf(false) }
 
     val previewImageUrl = scene.spriteUrl
     val imageLoader = SingletonImageLoader.get(LocalPlatformContext.current)
@@ -305,7 +308,32 @@ fun PlaybackOverlay(
                 }
             }
         }
-        AnimatedVisibility(seekPreviewEnabled && seekBarFocused && seekProgress >= 0) {
+
+        if (isNotTvDevice) {
+            // Playback will be paused while dragging, but want to resume if it was playing
+            var wasPlaying by remember { mutableStateOf(true) }
+            LaunchedEffect(seekBarInteractionSource) {
+                seekBarInteractionSource.interactions.collect {
+                    when (it) {
+                        is DragInteraction.Start -> {
+                            seekBarDragging = true
+                            controllerViewState.pulseControls(Int.MAX_VALUE)
+                            wasPlaying = playerControls.isPlaying
+                            playerControls.pause()
+                        }
+
+                        is DragInteraction.Stop,
+                        is DragInteraction.Cancel,
+                        -> {
+                            seekBarDragging = false
+                            controllerViewState.pulseControls()
+                            if (wasPlaying) playerControls.play()
+                        }
+                    }
+                }
+            }
+        }
+        AnimatedVisibility(seekPreviewEnabled && seekProgress >= 0 && (seekBarFocused || seekBarDragging)) {
             LaunchedEffect(Unit) {
                 seekProgress = playerControls.currentPosition.toFloat() / playerControls.duration
             }
