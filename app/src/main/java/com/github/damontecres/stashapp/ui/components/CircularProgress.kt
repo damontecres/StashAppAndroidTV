1e1a6fdadc2e232104adc21ebd9642089ff6c278
Damontecres <damontecres@gmail.com>
2026-07-26 21:45:03 -0400

    Add loading & error composables

--- app/src/main/java/com/github/damontecres/stashapp/ui/components/CircularProgress.kt
+++ app/src/main/java/com/github/damontecres/stashapp/ui/components/CircularProgress.kt
@@ -1,13 +1,21 @@
 package com.github.damontecres.stashapp.ui.components
 
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.unit.dp
 import androidx.tv.material3.MaterialTheme
 import com.github.damontecres.stashapp.ui.Material3AppTheme
+import com.github.damontecres.stashapp.ui.tryRequestFocus
 import com.github.damontecres.stashapp.ui.util.ifElse
 
 @Composable
@@ -22,5 +30,33 @@ fun CircularProgress(
                 modifier = Modifier.align(Alignment.Center),
             )
         }
+    }
+}
+
+/**
+ * Fill the space with a loading indicator and take focus
+ */
+@Composable
+fun LoadingPage(
+    modifier: Modifier = Modifier,
+    focusEnabled: Boolean = true,
+) {
+    val focusRequester = remember { FocusRequester() }
+    LaunchedEffect(Unit) { focusRequester.tryRequestFocus() }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier =
+            modifier
+                .fillMaxSize()
+                .focusRequester(focusRequester)
+                .focusable(focusEnabled),
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.border,
+            modifier =
+                Modifier
+                    .align(Alignment.Center)
+                    .size(48.dp),
+        )
     }
 }
